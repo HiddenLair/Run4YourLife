be4d694c6b31adef126c48edca7840ec3adb93dc
Pere Viader Masuet <pere.viader22@gmail.com>
2018-01-19 12:50:22 +0100

    Added text mesh pro and basic scene implementation

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Videogame made for the UPC master's degree in videogame creation and design
 ## Website
 
 https://hiddenlair.github.io/Run4YourLife/
+
+
+Unity version: Unity 2017.3.0f3
---
+++ Run4YourLife/Assets/Code/Main Menu/MainMenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Run4YourLife.MainMenu
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        public void OnPlayButtonPressed()
+        {
+            Debug.Log("OnPlayButtonPressed");
+        }
+
+        public void OnOptionsButtonPressed()
+        {
+            Debug.Log("OnOptionsButtonPressed");
+        }
+
+        public void OnExitButtonPressed()
+        {
+            Debug.Log("OnExitButtonPressed");
+        }
+    }
+}
