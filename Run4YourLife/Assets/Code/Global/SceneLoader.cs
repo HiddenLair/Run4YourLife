1dbd94a5a7d7de2ca341f0f2200f19fcd1b90c79
Pere Viader Masuet <pere.viader22@gmail.com>
2018-01-21 13:39:06 +0100

    Added scene loader for the global scene to load main menu

---
+++ Run4YourLife/Assets/Code/Global/SceneLoader.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Run4YourLife.SceneManagement
+{
+    public enum LoadEvent
+    {
+        Custom,
+        Awake,
+        Start,
+        OnEnable,
+        OnDisable,
+        Destroyed,
+        OnDestroy
+    }
+
+    public class SceneLoader : MonoBehaviour
+    {
+        [SerializeField]
+        private string sceneName;
+
+        [SerializeField]
+        private LoadSceneMode loadSceneMode;
+
+        [SerializeField]
+        private LoadEvent loadEvent;
+
+        void Awake()
+        {
+            if (loadEvent.Equals(LoadEvent.Awake))
+            {
+                LoadScene();
+            }
+        }
+
+        void Start()
+        {
+            if (loadEvent.Equals(LoadEvent.Start))
+            {
+                LoadScene();
+            }
+        }
+
+
+        void OnEnable()
+        {
+            if (loadEvent.Equals(LoadEvent.OnEnable))
+            {
+                LoadScene();
+            }
+        }
+
+        void OnDisable()
+        {
+            if (loadEvent.Equals(LoadEvent.OnDisable))
+            {
+                LoadScene();
+            }
+        }
+
+        void OnDestroy()
+        {
+            if (loadEvent.Equals(LoadEvent.OnDestroy))
+            {
+                LoadScene();
+            }
+        }
+
+        public void LoadScene()
+        {
+            SceneManager.LoadScene(sceneName, loadSceneMode);
+        }
+    }
+}
