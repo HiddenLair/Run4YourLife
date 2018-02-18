fd3f2765f6a4e35ec8ca84be971b4b1b70298559
ZelleryonDesktop <xavibravo.guillen@gmail.com>
2018-02-18 18:35:44 +0100

    Added traps scene (Lethal Trap and Impact Trap) - First steps

---
+++ Run4YourLife/Assets/Code/Interfaces/EventMessages/EventMessageTarget.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public interface IEventMessageTarget : IEventSystemHandler
+{
+    void Explosion();
+    void Impulse(Vector3 force);
+    //void Impulse(Vector3 force);
+    //void Burned(int time);
+    //void Slow(int time);
+}
---
+++ Run4YourLife/Assets/Code/Traps/ImpactTrap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ImpactTrap : MonoBehaviour
+{
+    public float areaOfEffectRadius = 2.0f;
+    public LayerMask trapListener;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Collider[] collisions = Physics.OverlapSphere(transform.position, areaOfEffectRadius, trapListener);
+        foreach (Collider c in collisions)
+        {
+            Vector3 direction = c.gameObject.GetComponent<Collider>().bounds.center - transform.position;
+            ExecuteEvents.Execute<IEventMessageTarget>(c.gameObject, null, (x, y) => x.Impulse(direction.normalized));
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, areaOfEffectRadius);
+    }
+}
