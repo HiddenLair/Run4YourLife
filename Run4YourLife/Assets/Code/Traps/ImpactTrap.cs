cea604cdba86f8c6727d442e5fa3ed38d58ca1f6
Zelleryon <xavibravo.guillen@gmail.com>
2018-02-19 19:18:27 +0100

    Added ground check to traps to avoid hitting hidden characters

--- Run4YourLife/Assets/Code/Traps/ImpactTrap.cs
+++ Run4YourLife/Assets/Code/Traps/ImpactTrap.cs
@@ -5,16 +5,22 @@ using UnityEngine.EventSystems;
 
 public class ImpactTrap : MonoBehaviour
 {
+    #region Public variables
     public float areaOfEffectRadius = 2.0f;
     public LayerMask trapListener;
+    public LayerMask blockingElement;
+    #endregion
 
     private void OnTriggerEnter(Collider collider)
     {
         Collider[] collisions = Physics.OverlapSphere(transform.position, areaOfEffectRadius, trapListener);
-        foreach (Collider c in collisions)
+        foreach( Collider c in collisions )
         {
-            Vector3 direction = c.gameObject.GetComponent<Collider>().bounds.center - transform.position;
-            ExecuteEvents.Execute<IEventMessageTarget>(c.gameObject, null, (x, y) => x.Impulse(direction.normalized));
+            if (!Physics.Linecast(transform.position, c.gameObject.GetComponent<Collider>().bounds.center, blockingElement))
+            {
+                Vector3 direction = c.gameObject.GetComponent<Collider>().bounds.center - transform.position;
+                ExecuteEvents.Execute<IEventMessageTarget>(c.gameObject, null, (x, y) => x.Impulse(direction.normalized));
+            }
         }
     }
 
