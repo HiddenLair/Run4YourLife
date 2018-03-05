4d0659fd6ba09b9145386dbc1b225f232ae6c5be
Pere Viader <pere.viader22@gmail.com>
2018-03-05 14:25:45 +0100

    Solved bug with scene loading #74

---
+++ Run4YourLife/Assets/DebugEventTriggerer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DebugEventTriggerer : MonoBehaviour {
+    public UnityEvent events;
+    public KeyCode key;
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(key))
+        {
+            events.Invoke();
+        }
+    }
+}
