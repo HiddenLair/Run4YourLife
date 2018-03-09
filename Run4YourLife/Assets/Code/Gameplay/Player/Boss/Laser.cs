a6d4a90076d46461cb93bd9ad30ce4c597e6cfa3
ZelleryonDesktop <xavibravo.guillen@gmail.com>
2018-03-09 19:46:33 +0100

    Added local CD for Boss Traps (Different to the Global Trap Cast CD)

--- Run4YourLife/Assets/Code/Gameplay/Player/Boss/Laser.cs
+++ Run4YourLife/Assets/Code/Gameplay/Player/Boss/Laser.cs
@@ -21,7 +21,14 @@ public class Laser : MonoBehaviour {
     public GameObject skillB;
     public GameObject trapB;
     public float trapDelaySpawn;
+
+    #region Public Trap CD
     public float globalTrapCD = 1.5f;
+    public float trapACD = 5.0f;
+    public float trapBCD = 5.0f;
+    public float trapYCD = 5.0f;
+    public float trapXCD = 5.0f;
+    #endregion
 
     public enum Phase {Phase1,Phase2,Phase3 };
     private enum Type { TRAP,SKILL};
@@ -31,7 +38,15 @@ public class Laser : MonoBehaviour {
     private int yPosIndex = 0;
     private float lastYPos = 0;
     private float timerChangeBetweenY = 0.0f;
-    private float globalTrapCDTimer = 0.0f;
+
+    #region Private Trap CD Timers
+    private float globalTrapCD_Timer = 0.0f;
+    private float trapACD_Timer = 0.0f;
+    private float trapBCD_Timer = 0.0f;
+    private float trapYCD_Timer = 0.0f;
+    private float trapXCD_Timer = 0.0f;
+    #endregion
+
 
     [HideInInspector]
     public bool isReadyForAction = true; //needed also for boss scripts
@@ -43,12 +58,21 @@ public class Laser : MonoBehaviour {
         bossControlScheme = GetComponent<BossControlScheme>();
         anim = GetComponent<Animator>();
         setIndex = (int)phase % sets.Length;
+        trapACD_Timer = trapACD;
+        trapBCD_Timer = trapBCD;
+        trapXCD_Timer = trapXCD;
+        trapYCD_Timer = trapYCD;
     }
 
     void Update ()
     {
+        //Add deltaTime to CD Timers
+        globalTrapCD_Timer += Time.deltaTime;
+        trapACD_Timer += Time.deltaTime;
+        trapBCD_Timer += Time.deltaTime;
+        trapXCD_Timer += Time.deltaTime;
+        trapYCD_Timer += Time.deltaTime;
 
-        globalTrapCDTimer += Time.deltaTime;
         isReadyForAction = anim.GetCurrentAnimatorStateInfo(0).IsName("move");
 
         MoveX(laser);
@@ -160,22 +184,26 @@ public class Laser : MonoBehaviour {
     }
     void SetTraps()
     {
-        if (globalTrapCDTimer > globalTrapCD)
+        if (globalTrapCD_Timer > globalTrapCD)
         {
-            if (bossControlScheme.skill1.Started())
+            if (bossControlScheme.skill1.Started() && (trapACD_Timer > trapACD))
             {
+                trapACD_Timer = 0.0f;
                 SetElement(trapA, skillA);
             }
-            else if (bossControlScheme.skill2.Started())
-            {               
+            else if (bossControlScheme.skill2.Started() && (trapXCD_Timer > trapXCD))
+            {
+                trapXCD_Timer = 0.0f;
                 SetElement(trapX, skillX);
             }
-            else if (bossControlScheme.skill3.Started())
+            else if (bossControlScheme.skill3.Started() && (trapYCD_Timer > trapYCD))
             {
+                trapYCD_Timer = 0.0f;
                 SetElement(trapY, skillY);
             }
-            else if (bossControlScheme.skill4.Started())
+            else if (bossControlScheme.skill4.Started() && (trapBCD_Timer > trapBCD))
             {
+                trapBCD_Timer = 0.0f;
                 SetElement(trapB, skillB);
             }
         }
@@ -183,7 +211,7 @@ public class Laser : MonoBehaviour {
 
     void SetElement(GameObject trap, GameObject skill)
     {
-        globalTrapCDTimer = 0.0f;
+        globalTrapCD_Timer = 0.0f;
 
         anim.SetTrigger("Casting");
         isReadyForAction = false;
