bbbd778cbd08b0482fa88a11e8d7593fab55e5f5
ZelleryonDesktop <xavibravo.guillen@gmail.com>
2018-03-11 20:52:23 +0100

    Added SkillControl script and Burned state to character

---
+++ Run4YourLife/Assets/Code/Skills/SkillControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum SkillType { FIRE };
+
+public class SkillControl : MonoBehaviour
+{
+    #region Public variables
+    public SkillType skilltype;
+    #endregion
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        switch(skilltype)
+        {
+            case SkillType.FIRE:
+                ExecuteEvents.Execute<IEventMessageTarget>(collider.gameObject, null, (x, y) => x.Burned());
+                break;
+        }
+    }
+}
