1d5280e2ca2606c11f04a5d433a20d2d730a5814
Zelleryon <xavibravo.guillen@gmail.com>
2018-03-12 21:23:46 +0100

    Refractored Fire Skill

---
+++ Run4YourLife/Assets/Code/Skills/FireSkillControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FireSkillControl : MonoBehaviour {
+
+    #region Public variables
+    public int burningTime = 5;
+    #endregion
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Player")
+        ExecuteEvents.Execute<IEventMessageTarget>(collider.gameObject, null, (x, y) => x.Burned(burningTime));     
+    }
+}
