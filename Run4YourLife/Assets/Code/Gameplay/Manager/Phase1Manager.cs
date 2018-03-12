6215e0d0cdea9cc8c7aa85719e20165b41eab6a5
Pere Viader <pere.viader22@gmail.com>
2018-03-12 15:03:26 +0100

    Added phase managers

---
+++ Run4YourLife/Assets/Code/Gameplay/Manager/Phase1Manager.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+using Run4YourLife.Player;
+
+namespace Run4YourLife.GameManagement
+{
+    public class Phase1Manager : GamePhaseManager
+    {
+        #region Member variables
+
+        [SerializeField]
+        private GameObject blueRunnerPrefab;
+
+        [SerializeField]
+        private GameObject redRunnerPrefab;
+
+        [SerializeField]
+        private GameObject greenRunnerPrefab;
+
+        [SerializeField]
+        private GameObject orangeRunnerPrefab;
+
+        [SerializeField]
+        private GameObject bossPrefab;
+
+        [SerializeField]
+        private Transform[] runnerSpawn;
+
+        [SerializeField]
+        private Transform spawnLocationBoss;
+
+        [SerializeField]
+        private GameObject m_phase1to2Bridge;
+
+        [SerializeField]
+        private GameObject m_phase2StartTrigger;
+
+        #endregion
+
+        #region Initialization
+
+        private void Awake()
+        {
+            RegisterPhase(GamePhase.Phase1);
+        }
+
+        #endregion
+
+        #region Regular Execution
+
+        public override void StartPhase()
+        {
+            PlayerManager playerManager = FindObjectOfType<PlayerManager>();
+            Debug.Assert(playerManager != null);
+
+            InstantiatePlayers(playerManager.GetPlayers());
+
+            m_phase1to2Bridge.SetActive(true);
+            m_phase2StartTrigger.SetActive(true);
+        }
+
+        public override void EndPhase()
+        {
+            Debug.Log("Boss should play exit animation");
+            GameObject boss = FindObjectOfType<Boss>().gameObject;
+            Debug.Assert(boss != null);
+            Destroy(boss);
+        }
+
+        private void InstantiatePlayers(List<PlayerDefinition> playerDefinitions)
+        {
+            foreach (PlayerDefinition playerDefinition in playerDefinitions)
+            {
+                InstantiatePlayer(playerDefinition);
+            }
+        }
+
+        private void InstantiatePlayer(PlayerDefinition playerDefinition)
+        {
+            GameObject player = InstantiatePlayerByType(playerDefinition);
+            ExecuteEvents.Execute<IPlayerDefinitionEvents>(player, null, (a, b) => a.OnPlayerDefinitionChanged(playerDefinition));
+        }
+
+        private GameObject InstantiatePlayerByType(PlayerDefinition playerDefinition)
+        {
+            GameObject instance;
+
+            if (playerDefinition.IsBoss)
+            {
+                instance = Instantiate(bossPrefab, spawnLocationBoss.position, spawnLocationBoss.rotation);
+                Camera.main.GetComponent<CameraBossFollow>().boss = instance.transform; // TODO: Temporal camera attachment
+            }
+            else
+            {
+                GameObject runnerPrefab = null;
+                switch (playerDefinition.CharacterType)
+                {
+                    case CharacterType.Blue:
+                        runnerPrefab = bossPrefab;
+                        break;
+                    case CharacterType.Green:
+                        runnerPrefab = greenRunnerPrefab;
+                        break;
+                    case CharacterType.Orange:
+                        runnerPrefab = orangeRunnerPrefab;
+                        break;
+                    case CharacterType.Red:
+                        runnerPrefab = redRunnerPrefab;
+                        break;
+                }
+                // TODO implementation of spawn that does not require 3 transforms instead of 4
+                Transform spawn = runnerSpawn[playerDefinition.ID - 1];
+                instance = Instantiate(runnerPrefab, spawn.position, spawn.rotation);
+            }
+
+            return instance;
+        }
+
+        #endregion
+
+        #region Debug Execution
+
+        public override void DebugEndPhase()
+        {
+            GameObject boss = FindObjectOfType<Boss>().gameObject;
+            Debug.Assert(boss != null);
+            Destroy(boss);
+
+            PlayerCharacterController[] players = FindObjectsOfType<PlayerCharacterController>();
+            foreach (PlayerCharacterController player in players)
+            {
+                Destroy(player.gameObject);
+            }
+        }
+
+        public override void DebugStartPhase()
+        {
+            StartPhase();
+        }
+
+        #endregion
+    }
+}
