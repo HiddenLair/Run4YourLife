312692b4bff0a7486e554b58f6ecbcd6ede2e501
Moaif <32565030+Moaif@users.noreply.github.com>
2018-03-14 15:01:46 +0100

    Added collisions to fase1, and dangerPlatfrom logic

---
+++ Run4YourLife/Assets/Code/Generic/ChangeRigidBodyOnCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeRigidBodyOnCollision : MonoBehaviour {
+
+    public bool gravity;
+    public bool kinematic;
+    //TODO:: add more options
+
+    private Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Entra");
+        body.useGravity = gravity;
+        body.isKinematic = kinematic;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        body.useGravity = gravity;
+        body.isKinematic = kinematic;
+    }
+}
