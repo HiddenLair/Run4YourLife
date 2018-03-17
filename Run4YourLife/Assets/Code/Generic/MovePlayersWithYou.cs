75975021ca5e6079cdf5b642f68bff70aa7264bd
Moaif <32565030+Moaif@users.noreply.github.com>
2018-03-17 02:01:14 +0100

    Added platform spawners, totems and rockers logic

---
+++ Run4YourLife/Assets/Code/Generic/MovePlayersWithYou.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayersWithYou : MonoBehaviour {
+
+    public Vector3 speed;
+
+    List<Transform> players = new List<Transform>();
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (Transform transform in players)
+        {
+            transform.Translate(speed * Time.deltaTime, Space.World);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        players.Add(other.transform);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        players.Remove(other.transform);
+    }
+}
