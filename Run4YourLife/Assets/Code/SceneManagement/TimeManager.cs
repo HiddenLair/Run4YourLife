f1eeb65ff2f62dad442b383ebae377ad38c9dc60
Moaif <32565030+Moaif@users.noreply.github.com>
2018-03-17 19:19:41 +0100

    Working on transitions

---
+++ Run4YourLife/Assets/Code/SceneManagement/TimeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Run4YourLife.SceneManagement;
+using Run4YourLife.GameManagement;
+
+public class TimeManager : MonoBehaviour {
+
+    public float timeInPhase;
+
+    public GameObject sceneLoadRequests;
+
+    private void Start()
+    {
+        GetComponent<PlayerSpawner>().InstantiateBossPlayer();
+        GetComponent<PlayerSpawner>().InstantiatePlayers(); 
+    }
+
+    // Update is called once per frame
+    void Update () {
+        timeInPhase -= Time.deltaTime;
+        if(timeInPhase <= 0)
+        {
+            foreach (SceneLoadRequest request in sceneLoadRequests.GetComponents<SceneLoadRequest>())
+            {
+                request.Execute();
+            }
+        }
+	}
+}
