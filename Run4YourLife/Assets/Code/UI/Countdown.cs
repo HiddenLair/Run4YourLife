26335d1a570770e4f83310a245628c38906d443c
gerardpf2 <gerard.contacte@gmail.com>
2018-03-17 19:31:35 +0100

    Added ui countdown time for phase 2 and updated ui prefab

---
+++ Run4YourLife/Assets/Code/UI/Countdown.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    private Text text;
+
+    private bool on = false;
+
+    private float time = 0.0f;
+
+	void Awake()
+    {
+        text = GetComponent<Text>();
+	}
+
+	void Update()
+    {
+		if(on)
+        {
+            time -= Time.deltaTime;
+
+            if(time <= 0.0f)
+            {
+                on = false;
+                time = 0.0f;
+            }
+
+            text.text = Mathf.Round(time).ToString();
+        }
+	}
+
+    public void Go(float newTime)
+    {
+        on = true;
+        time = newTime;
+    }
+}
