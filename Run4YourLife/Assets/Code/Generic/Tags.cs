1e0d5db70d872a292f96d4d86921a6674b86cb71
Pere Viader <pere.viader22@gmail.com>
2018-03-18 19:10:59 +0100

    Added tags class

---
+++ Run4YourLife/Assets/Code/Generic/Tags.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tags : MonoBehaviour {
+    public readonly static string Untagged = "Untagged";
+    public readonly static string Respawn = "Respawn";
+    public readonly static string Finish = "Finish";
+    public readonly static string EditorOnly = "EditorOnly";
+    public readonly static string MainCamera = "MainCamera";
+    public readonly static string Player = "Player";
+    public readonly static string GameController = "Destructible";
+    public readonly static string Shoot = "Shoot";
+    public readonly static string Ground = "Ground";
+    public readonly static string Trap = "Trap";
+    public readonly static string Boss = "Boss";
+    public readonly static string Floor = "Floor";
+    public readonly static string Interactable = "Interactable";
+    public readonly static string DropPlatform = "Trap";
+    public readonly static string UI = "UI";
+}
