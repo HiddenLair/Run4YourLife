af628e9dff8c04f2b1d5b85471fff2f5d67f3a4c
gerardpf2 <gerard.contacte@gmail.com>
2018-03-27 18:06:43 +0200

    Added debug phase switcher

---
+++ Run4YourLife/Assets/Code/Debug/Other/PhaseSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+using Run4YourLife.Utils;
+using Run4YourLife.GameManagement;
+
+namespace Run4YourLife.DebuggingTools
+{
+    public class PhaseSwitcher : MonoBehaviour
+    {
+        private GameManager gameManager = null;
+
+        void Start()
+        {
+            gameManager = FindObjectOfType<GameManager>();
+        }
+
+        public void GoPhase1()
+        {
+            StartCoroutine(YieldHelper.SkipFrame(gameManager.DebugEndExecutingPhaseAndDebugStartPhase, GamePhase.EasyMoveHorizontal));
+        }
+
+        public void GoPhase2()
+        {
+            StartCoroutine(YieldHelper.SkipFrame(gameManager.DebugEndExecutingPhaseAndDebugStartPhase, GamePhase.BossFight));
+        }
+
+        public void GoPhase3()
+        {
+            StartCoroutine(YieldHelper.SkipFrame(gameManager.DebugEndExecutingPhaseAndDebugStartPhase, GamePhase.HardMoveHorizontal));
+        }
+    }
+}
