80488d14e008139fdf595ab726d6bc86623404de
Moaif <32565030+Moaif@users.noreply.github.com>
2018-04-03 13:18:07 +0200

    Added AbsoluteKill event to differenciate from kill

--- Run4YourLife/Assets/Code/Gameplay/Player/Runner/ICharacterEvents.cs
+++ Run4YourLife/Assets/Code/Gameplay/Player/Runner/ICharacterEvents.cs
@@ -4,6 +4,7 @@ using UnityEngine.EventSystems;
 public interface ICharacterEvents : IEventSystemHandler
 {
     void Kill();
+    void AbsoluteKill();
     void Impulse(Vector3 direction,float force);
     void Root(int rootHardness);
     void Debuff(StatModifier statsModifier);
---
+++ Run4YourLife/Assets/Code/Gameplay/Utils/AbsoluteEliminatePlayerOnTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AbsoluteEliminatePlayerOnTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider triggered)
+    {
+        if (triggered.CompareTag(Tags.Player))
+        {
+            ExecuteEvents.Execute<ICharacterEvents>(triggered.gameObject, null, (x, y) => x.AbsoluteKill());
+        }
+    }
+
+    private void OnCollisionEnter(Collision collided)
+    {
+        if (collided.gameObject.CompareTag(Tags.Player))
+        {
+            ExecuteEvents.Execute<ICharacterEvents>(collided.gameObject, null, (x, y) => x.AbsoluteKill());
+        }
+    }
+}
