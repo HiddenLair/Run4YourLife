e2f942aafa1f4ae4dcacdb193cbf68167d4ecd1a
Pere Viader <pere.viader22@gmail.com>
2018-04-08 15:26:21 +0200

    Renamed player killing scripts to runner killing scripts #156

---
+++ Run4YourLife/Assets/Code/Gameplay/Player/Runner/KillRunnerOnPhisicInteraction.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class KillRunnerOnPhisicInteraction : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.CompareTag(Tags.Player))
+        {
+            SendKillEvent(collider.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag(Tags.Player))
+        {
+            SendKillEvent(collision.gameObject);
+        }
+    }
+
+    void SendKillEvent(GameObject runner)
+    {
+        ExecuteEvents.Execute<ICharacterEvents>(runner, null, (x, y) => x.Kill());
+    }
+}
