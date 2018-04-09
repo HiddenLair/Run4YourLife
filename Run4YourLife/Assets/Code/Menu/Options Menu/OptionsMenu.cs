56270bbe5a537e2b623c21c3cfe12c2736c51f17
ZelleryonDesktop <xavibravo.guillen@gmail.com>
2018-04-09 02:05:01 +0200

    Updated Options menu

---
+++ Run4YourLife/Assets/Code/Menu/Options Menu/OptionsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+    #region Private Variables
+    private Resolution[] availableResolutions;
+    #endregion
+
+    #region Public Variables
+    #endregion
+
+    void Start ()
+    {
+        availableResolutions = Screen.resolutions;
+	}
+	
+}
