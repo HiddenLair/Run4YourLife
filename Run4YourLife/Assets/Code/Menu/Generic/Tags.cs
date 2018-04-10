362496cf04972fb9d138ef0c349fff0d35f3c733
Pere Viader <pere.viader22@gmail.com>
2018-04-10 14:15:07 +0200

    Removed trap Tag

--- Run4YourLife/Assets/Code/Menu/Generic/Tags.cs
+++ Run4YourLife/Assets/Code/Menu/Generic/Tags.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tags : MonoBehaviour {
+    //Unity Tags
     public readonly static string Untagged = "Untagged";
     public readonly static string Respawn = "Respawn";
     public readonly static string Finish = "Finish";
     public readonly static string EditorOnly = "EditorOnly";
     public readonly static string MainCamera = "MainCamera";
     public readonly static string Runner = "Player";
+    public readonly static string GameController = "GameController";
+
+    //Custom Tags
     public readonly static string Shoot = "Shoot";
-    public readonly static string Trap = "Trap";
     public readonly static string Boss = "Boss";
     public readonly static string Interactable = "Interactable";
     public readonly static string UI = "UI";
