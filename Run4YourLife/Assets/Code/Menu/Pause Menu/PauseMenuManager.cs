7fb5b3c6d3a3d2f368e50dda6d27420df30059a8
ZelleryonDesktop <xavibravo.guillen@gmail.com>
2018-04-13 20:10:44 +0200

    Added Pause Screen

---
+++ Run4YourLife/Assets/Code/Menu/Pause Menu/PauseMenuManager.cs
@@ -0,0 +1,25 @@
+﻿using Run4YourLife.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Run4YourLife.PauseMenu
+{
+    public class PauseMenuManager : MonoBehaviour
+    {
+        [SerializeField]
+        private SceneLoadRequest m_mainMenuLoadRequest;
+
+        [SerializeField]
+        private SceneLoadRequest m_gameSceneLoadRequest;
+
+        [SerializeField]
+        private SceneLoadRequest m_optionsMenuLoadRequest;
+
+        public void OnExitButtonPressed()
+        {
+            m_mainMenuLoadRequest.Execute();
+        }
+    }
+}
