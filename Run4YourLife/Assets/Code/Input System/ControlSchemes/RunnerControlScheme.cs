1769e82b804209b351be07a28e76d6e4721a535f
Moaif <32565030+Moaif@users.noreply.github.com>
2018-04-15 17:01:03 +0200

    Working on rock launcher

--- Run4YourLife/Assets/Code/Input System/ControlSchemes/RunnerControlScheme.cs
+++ Run4YourLife/Assets/Code/Input System/ControlSchemes/RunnerControlScheme.cs
@@ -8,6 +8,7 @@ namespace Run4YourLife.Input
         public Action move;
         public Action jump;
         public Action interact;
+        public Action rock;
 
         private void Awake()
         {
@@ -15,6 +16,7 @@ namespace Run4YourLife.Input
             actions.Add(move = new Action(new InputSource(Axis.LEFT_HORIZONTAL)));
             actions.Add(jump = new Action(new InputSource(Button.A)));
             actions.Add(interact = new Action(new InputSource(Button.X)));
+            actions.Add(rock = new Action(new InputSource(Button.B)));
         }
     }
 }
