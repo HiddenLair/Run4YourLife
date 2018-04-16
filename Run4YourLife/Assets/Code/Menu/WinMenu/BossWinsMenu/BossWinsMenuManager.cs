7dfc3af05d836dd78a39d1096d35ddd19ce89754
gerardpf2 <gerard.contacte@gmail.com>
2018-04-17 00:10:00 +0200

    Added BossWinsMenu

---
+++ Run4YourLife/Assets/Code/Menu/WinMenu/BossWinsMenu/BossWinsMenuManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Run4YourLife.WinMenu
+{
+    public class BossWinsMenuManager : WinMenuManager
+    {
+        [SerializeField]
+        private Animator bossAnimator;
+
+        [SerializeField]
+        private string bossAnimation = "Laugh";
+
+        void Awake()
+        {
+            bossAnimator.Play(bossAnimation);
+        }
+    }
+}
