b356733de470e4cba8615179bded42168066bb36
BravoXavi <BravoXavi@users.noreply.github.com>
2018-04-17 15:39:15 +0200

    Added PauseEventHandler to the character prefabs and Pause Manager to the Game Scene

---
+++ Run4YourLife/Assets/Code/Input System/PauseEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using Run4YourLife.Input;
+using UnityEngine.EventSystems;
+using Run4YourLife.GameManagement;
+
+namespace Run4YourLife.Input
+{
+    [RequireComponent(typeof(PlayerControlScheme))]
+    class PauseEventHandler : MonoBehaviour
+    {
+        private PlayerControlScheme m_playerControlScheme;
+
+        void Awake()
+        {
+            m_playerControlScheme = GetComponent<PlayerControlScheme>();
+        }
+
+        private void Update()
+        {
+            CheckForPauseRequest();
+        }
+
+        private void CheckForPauseRequest()
+        {
+            bool pauseReq = m_playerControlScheme.Pause.Started();
+
+            if(pauseReq)
+            {
+                ExecuteEvents.Execute<IPauseEvent>(gameObject, null, (x, y) => x.OnPauseInput());
+            }
+        }
+    }
+}
