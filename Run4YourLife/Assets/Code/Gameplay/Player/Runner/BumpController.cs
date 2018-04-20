4071c3e374623e48fddf7fabcc0e6d784496980b
Moaif <32565030+Moaif@users.noreply.github.com>
2018-04-20 16:56:14 +0200

    Added bump by script and removed animated one

---
+++ Run4YourLife/Assets/Code/Gameplay/Player/Runner/BumpController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumpController : MonoBehaviour {
+
+    #region Inspector
+    [SerializeField]
+    private SkinnedMeshRenderer rightEye;
+
+    [SerializeField]
+    private SkinnedMeshRenderer leftEye;
+
+    [SerializeField]
+    private SkinnedMeshRenderer head;
+
+    [SerializeField]
+    private float bumpTime = 0.5f;
+
+    #endregion
+
+    public void Bump()
+    {
+        StartCoroutine(BumpInTime());
+    }
+
+    IEnumerator BumpInTime()
+    {      
+        float growTime = bumpTime/2.0f;
+        while (growTime >= 0)
+        {
+            float weight = 100* (1-(growTime / (bumpTime/2.0f)));
+            leftEye.SetBlendShapeWeight(0, weight);
+            rightEye.SetBlendShapeWeight(0, weight);
+            head.SetBlendShapeWeight(0, weight);
+            growTime -= Time.deltaTime;
+            yield return null;
+        }
+
+        float decreaseTime =bumpTime/2.0f;
+        while (decreaseTime >= 0)
+        {
+            float weight = 100 * (decreaseTime / (bumpTime/2.0f));
+            leftEye.SetBlendShapeWeight(0, weight);
+            rightEye.SetBlendShapeWeight(0, weight);
+            head.SetBlendShapeWeight(0, weight);
+            decreaseTime -= Time.deltaTime;
+            yield return null;
+        }
+    }
+
+}
