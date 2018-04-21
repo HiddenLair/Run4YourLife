f8e9f32744d0cc51f9792b625524828dc8c4dfee
gerardpf2 <gerard.contacte@gmail.com>
2018-04-21 17:51:31 +0200

    character selection no more than one runner of the same color allowed

--- Run4YourLife/Assets/Code/Gameplay/Manager/PlayerManager.cs
+++ Run4YourLife/Assets/Code/Gameplay/Manager/PlayerManager.cs
@@ -58,8 +58,55 @@ namespace Run4YourLife.Player
 
         public void SetPlayerAsRunner(PlayerHandle player)
         {
+            player.CharacterType = GetFirstAviablePlayerCharacterType();
             player.IsBoss = false;
             OnPlayerChanged.Invoke(player);
+        }
+
+        public CharacterType GetFirstAviablePlayerCharacterType()
+        {
+            return GetFirstAviableCharacterType(CharacterType.Purple, 1);
+        }
+
+        public CharacterType GetFirstAviableCharacterType(CharacterType characterType, int direction)
+        {
+            CharacterType ret = characterType;
+
+            int characterTypeI = (int)characterType;
+            int characterTypeCount = Enum.GetValues(typeof(CharacterType)).Length;
+
+            for(int i = 0; i < characterTypeCount; ++i)
+            {
+                int currentCharacterTypeI = characterTypeI + direction * i;
+
+                currentCharacterTypeI %= characterTypeCount;
+                if(currentCharacterTypeI < 0)
+                {
+                    currentCharacterTypeI += characterTypeCount;
+                }
+
+                CharacterType currentCharacterType = (CharacterType)currentCharacterTypeI;
+
+                if(!PlayerHasCharacterType(currentCharacterType))
+                {
+                    ret = currentCharacterType;
+                    break;
+                }
+            }
+
+            return ret;
+        }
+
+        private bool PlayerHasCharacterType(CharacterType characterType)
+        {
+            foreach(PlayerHandle player in GetRunners())
+            {
+                if(player.CharacterType.Equals(characterType))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public void AddPlayer(PlayerHandle playerDefinition)
