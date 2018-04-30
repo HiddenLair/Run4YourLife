4491732afe6c8f7a858555b2cfce018478c805a6
Pere Viader <pere.viader22@gmail.com>
2018-04-30 20:41:58 +0200

    Namespace

--- Run4YourLife/Assets/Code/Gameplay/Player/Runner/RunnerController.cs
+++ Run4YourLife/Assets/Code/Gameplay/Player/Runner/RunnerController.cs
@@ -1,10 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
-using System;
-using Run4YourLife.Input;
-using Run4YourLife.Player;
+
 using Run4YourLife.GameManagement;
 
 
