ec415124b931cd246e019dc1edcd1b3c07fc4015
Pere Viader <pere.viader22@gmail.com>
2018-05-06 13:21:31 +0200

    Namespaces

--- Run4YourLife/Assets/Code/Player/DisableByBossProximity.cs
+++ Run4YourLife/Assets/Code/Player/DisableByBossProximity.cs
@@ -1,80 +1,83 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+
 using Run4YourLife.GameManagement;
-using UnityEngine.Rendering;
 
-public class DisableByBossProximity : BossDestructedInstance {
+namespace Run4YourLife.Interactables
+{
+    public class DisableByBossProximity : BossDestructedInstance {
 
-    [SerializeField]
-    private float m_alphaAnimationLenght = 0.5f;
+        [SerializeField]
+        private float m_alphaAnimationLenght = 0.5f;
 
-    private Renderer[] m_renderer;
+        private Renderer[] m_renderer;
 
-    private void Awake()
-    {
-        m_renderer = GetComponentsInChildren<Renderer>();
-    }
-
-    public override void OnBossDestroy()
-    {
-        foreach (Renderer renderer in m_renderer)
+        private void Awake()
         {
-            MakeTransparent(renderer.material);
-            StartCoroutine(BeautifullDestroy(m_alphaAnimationLenght, renderer.material));
+            m_renderer = GetComponentsInChildren<Renderer>();
         }
-    }
 
-    public override void OnRegenerate()
-    {
-        StopAllCoroutines();
-        gameObject.SetActive(true);
-
-        foreach (Renderer renderer in m_renderer)
+        public override void OnBossDestroy()
         {
-            Color color = renderer.material.color;
-            color.a = 1;
-            renderer.material.color = color;
-            MakeOpaque(renderer.material);
+            foreach (Renderer renderer in m_renderer)
+            {
+                MakeTransparent(renderer.material);
+                StartCoroutine(BeautifullDestroy(m_alphaAnimationLenght, renderer.material));
+            }
         }
-    }
 
-    private IEnumerator BeautifullDestroy(float trasitionLenght, Material mat)
-    {
-        enabled = false;
-
-        float time = trasitionLenght;
-        Color color = mat.color;
-        while (time >= 0)
+        public override void OnRegenerate()
         {
-            color.a = time / trasitionLenght;
-            mat.color = color;
-            yield return null;
-            time -= Time.deltaTime;
+            StopAllCoroutines();
+            gameObject.SetActive(true);
+
+            foreach (Renderer renderer in m_renderer)
+            {
+                Color color = renderer.material.color;
+                color.a = 1;
+                renderer.material.color = color;
+                MakeOpaque(renderer.material);
+            }
         }
-        gameObject.SetActive(false);
-    }
 
-    public void MakeTransparent(Material material)
-    {
-        material.SetFloat("_Mode", 3);
-        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-        material.SetInt("_ZWrite", 0);
-        material.DisableKeyword("_ALPHATEST_ON");
-        material.DisableKeyword("_ALPHABLEND_ON");
-        material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
-        material.renderQueue = 3000;
-    }
+        private IEnumerator BeautifullDestroy(float trasitionLenght, Material mat)
+        {
+            enabled = false;
 
-    private void MakeOpaque(Material material)
-    {
-        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-        material.SetInt("_ZWrite", 1);
-        material.DisableKeyword("_ALPHATEST_ON");
-        material.DisableKeyword("_ALPHABLEND_ON");
-        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-        material.renderQueue = -1;
+            float time = trasitionLenght;
+            Color color = mat.color;
+            while (time >= 0)
+            {
+                color.a = time / trasitionLenght;
+                mat.color = color;
+                yield return null;
+                time -= Time.deltaTime;
+            }
+            gameObject.SetActive(false);
+        }
+
+        public void MakeTransparent(Material material)
+        {
+            material.SetFloat("_Mode", 3);
+            material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+            material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            material.SetInt("_ZWrite", 0);
+            material.DisableKeyword("_ALPHATEST_ON");
+            material.DisableKeyword("_ALPHABLEND_ON");
+            material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+            material.renderQueue = 3000;
+        }
+
+        private void MakeOpaque(Material material)
+        {
+            material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+            material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+            material.SetInt("_ZWrite", 1);
+            material.DisableKeyword("_ALPHATEST_ON");
+            material.DisableKeyword("_ALPHABLEND_ON");
+            material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            material.renderQueue = -1;
+        }
     }
 }
--- Run4YourLife/Assets/Code/SceneSpecific/GameScene/BossDestroy/BossDestructedInstance.cs
+++ Run4YourLife/Assets/Code/SceneSpecific/GameScene/BossDestroy/BossDestructedInstance.cs
@@ -1,27 +1,30 @@
 ﻿using UnityEngine;
-using Run4YourLife.GameManagement;
 
-public abstract class BossDestructedInstance : MonoBehaviour {
-
-    [SerializeField]
-    private float m_distance;
-
-    public float DestroyPosition { get { return transform.position.x + m_distance; } }
-    public float Distance { get { return m_distance; } }
-
-    protected virtual void Start()
+namespace Run4YourLife.GameManagement
+{
+    public abstract class BossDestructedInstance : MonoBehaviour
     {
-        BossDestructorManager.Instance.Add(this);
-    }
 
-    public abstract void OnBossDestroy();
+        [SerializeField]
+        private float m_distance;
 
-    public abstract void OnRegenerate();
+        public float DestroyPosition { get { return transform.position.x + m_distance; } }
+        public float Distance { get { return m_distance; } }
 
-    protected virtual void OnDrawGizmosSelected()
-    {
-        Vector3 position = transform.position;
-        position.x = DestroyPosition;
-        Gizmos.DrawSphere(position, 0.5f);
+        protected virtual void Start()
+        {
+            BossDestructorManager.Instance.Add(this);
+        }
+
+        public abstract void OnBossDestroy();
+
+        public abstract void OnRegenerate();
+
+        protected virtual void OnDrawGizmosSelected()
+        {
+            Vector3 position = transform.position;
+            position.x = DestroyPosition;
+            Gizmos.DrawSphere(position, 0.5f);
+        }
     }
 }
