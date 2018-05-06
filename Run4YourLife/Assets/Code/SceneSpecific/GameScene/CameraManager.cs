dec702ff08f74c8feb19ffdcfe13e3150dd07be5
Pere Viader <pere.viader22@gmail.com>
2018-05-06 10:54:21 +0200

    Fixing references. Now Main Camera can be referenced at awake

--- Run4YourLife/Assets/Code/SceneSpecific/GameScene/CameraManager.cs
+++ Run4YourLife/Assets/Code/SceneSpecific/GameScene/CameraManager.cs
@@ -5,21 +5,32 @@ namespace Run4YourLife.GameManagement
 {
     public class CameraManager : SingletonMonoBehaviour<CameraManager> {
 
-        public Camera MainCamera { get { return m_camera; } }
-        public CinemachineBrain CinemachineBrain { get { return m_cinemachineBrain; } }
-        public ICinemachineCamera ActiveCinemachineCamera { get { return m_cinemachineBrain.ActiveVirtualCamera; } }
+        public ICinemachineCamera ActiveCinemachineCamera { get { return CinemachineBrain.ActiveVirtualCamera; } }
+
+        public Camera MainCamera {
+            get {
+                if(m_camera == null)
+                {
+                    m_camera = Camera.main;
+                    Debug.Assert(m_camera != null, "Main Camera Not Found");
+                }
+                return m_camera;
+            }
+        }
+
+        public CinemachineBrain CinemachineBrain {
+            get {
+                if (m_cinemachineBrain == null)
+                {
+                    m_cinemachineBrain = MainCamera.GetComponent<CinemachineBrain>();
+                    Debug.Assert(m_cinemachineBrain != null, "Cinemachine Brain Not Found");
+                }
+                return m_cinemachineBrain;
+            }
+        }
 
         private Camera m_camera;
         private CinemachineBrain m_cinemachineBrain;
-
-        private void Awake()
-        {
-            m_camera = Camera.main;
-            Debug.Assert(m_camera != null);
-
-            m_cinemachineBrain = m_camera.GetComponent<CinemachineBrain>();
-            Debug.Assert(m_cinemachineBrain != null);
-        }
 
         public void TransitionToCamera(ICinemachineCamera cinemachineCamera)
         {
