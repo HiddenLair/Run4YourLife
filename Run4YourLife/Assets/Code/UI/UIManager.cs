6080131afc1a85fd4995a0ef47239b473de1ed7f
Pere Viader <pere.viader22@gmail.com>
2018-05-11 11:39:58 +0200

    Changed pivots of UI actions

--- Run4YourLife/Assets/Code/UI/UIManager.cs
+++ Run4YourLife/Assets/Code/UI/UIManager.cs
@@ -1,121 +1,131 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿using System;
+
+using UnityEngine;
+
 using Run4YourLife.GameManagement;
 
 namespace Run4YourLife.UI
 {
+    #region Useful types for events
 
+    public enum ActionType
+    {
+        MELE, SHOOT,
+        TRAP_A, TRAP_B, TRAP_X, TRAP_Y,
+        SKILL_A, SKILL_B, SKILL_X, SKILL_Y
+    }
 
+    public enum SetType
+    {
+        TRAPS, SKILLS
+    }
+
+    public enum PhaseType
+    {
+        FIRST, SECOND, THIRD,
+        TRANSITION
+    }
+
+    #endregion
     public class UIManager : MonoBehaviour, IUIEvents
     {
-        #region Image updaters
+        #region  Editor
 
         [SerializeField]
-        private ImageUpdater imageUpdaterMele;
+        private UIBossActionController m_melee;
+        [SerializeField]
+        private UIBossActionController m_shoot;
 
         [SerializeField]
-        private ImageUpdater imageUpdaterShoot;
+        private UIBossActionController m_trapA;
 
         [SerializeField]
-        private ImageUpdater imageUpdaterTrapA;
+        private UIBossActionController m_trapB;
 
         [SerializeField]
-        private ImageUpdater imageUpdaterTrapB;
+        private UIBossActionController m_trapX;
 
         [SerializeField]
-        private ImageUpdater imageUpdaterTrapX;
+        private UIBossActionController m_trapY;
 
         [SerializeField]
-        private ImageUpdater imageUpdaterTrapY;
-
-        #endregion
-
-        #region ScaleTicks
+        private UIBossActionController m_skillA;
 
         [SerializeField]
-        private ScaleTick scaleTickMeleBottom;
+        private UIBossActionController m_skillB;
 
         [SerializeField]
-        private ScaleTick scaleTickMeleTop;
+        private UIBossActionController m_skillX;
 
         [SerializeField]
-        private ScaleTick scaleTickShootBottom;
-
-        [SerializeField]
-        private ScaleTick scaleTickShootTop;
-
-        [SerializeField]
-        private ScaleTick scaleTickTrapABottom;
-
-        [SerializeField]
-        private ScaleTick scaleTickTrapATop;
-
-        [SerializeField]
-        private ScaleTick scaleTickTrapBBottom;
-
-        [SerializeField]
-        private ScaleTick scaleTickTrapBTop;
-
-        [SerializeField]
-        private ScaleTick scaleTickTrapXBottom;
-
-        [SerializeField]
-        private ScaleTick scaleTickTrapXTop;
-
-        [SerializeField]
-        private ScaleTick scaleTickTrapYBottom;
-
-        [SerializeField]
-        private ScaleTick scaleTickTrapYTop;
-
-        #endregion
-
-        #region Progress
+        private UIBossActionController m_skillY;
 
         [SerializeField]
         private Progress progress;
-
-        #endregion
-
-        #region Countdown
 
         [SerializeField]
         private Countdown countdown;
 
         #endregion
 
-        private List<ImageUpdater> imageUpdaters = new List<ImageUpdater>();
+        UIBossActionController[] m_actionControllers;
 
-        private List<ScaleTick> scaleTicksBottom = new List<ScaleTick>();
-        private List<ScaleTick> scaleTicksTop = new List<ScaleTick>();
-
-        void Awake()
+        private void Awake()
         {
-            imageUpdaters.Add(imageUpdaterMele);
-            imageUpdaters.Add(imageUpdaterShoot);
-            imageUpdaters.Add(imageUpdaterTrapA);
-            imageUpdaters.Add(imageUpdaterTrapB);
-            imageUpdaters.Add(imageUpdaterTrapX);
-            imageUpdaters.Add(imageUpdaterTrapY);
+            
+            m_actionControllers = new  UIBossActionController[Enum.GetNames(typeof(ActionType)).Length];
+            m_actionControllers[(int)ActionType.MELE] = m_melee;
+            m_actionControllers[(int)ActionType.SHOOT] = m_shoot;
+            m_actionControllers[(int)ActionType.TRAP_A] = m_trapA;
+            m_actionControllers[(int)ActionType.TRAP_B] = m_trapB;
+            m_actionControllers[(int)ActionType.TRAP_X] = m_trapX;
+            m_actionControllers[(int)ActionType.TRAP_Y] = m_trapY;
+            m_actionControllers[(int)ActionType.SKILL_A] = m_skillA;
+            m_actionControllers[(int)ActionType.SKILL_B] = m_skillB;
+            m_actionControllers[(int)ActionType.SKILL_X] = m_skillX;
+            m_actionControllers[(int)ActionType.SKILL_Y] = m_skillY;
 
-            scaleTicksBottom.Add(scaleTickMeleBottom);
-            scaleTicksBottom.Add(scaleTickShootBottom);
-            scaleTicksBottom.Add(scaleTickTrapABottom);
-            scaleTicksBottom.Add(scaleTickTrapBBottom);
-            scaleTicksBottom.Add(scaleTickTrapXBottom);
-            scaleTicksBottom.Add(scaleTickTrapYBottom);
-
-            scaleTicksTop.Add(scaleTickMeleTop);
-            scaleTicksTop.Add(scaleTickShootTop);
-            scaleTicksTop.Add(scaleTickTrapATop);
-            scaleTicksTop.Add(scaleTickTrapBTop);
-            scaleTicksTop.Add(scaleTickTrapXTop);
-            scaleTicksTop.Add(scaleTickTrapYTop);
-
-            GameManager.Instance.onGamePhaseChanged.AddListener(ConversorTemporal);
+            GameManager.Instance.onGamePhaseChanged.AddListener(OnGamePhaseChanged);
         }
 
-        public void ConversorTemporal(GamePhase phase)//TODO: REFACTOR THIS
+        private void OnGamePhaseChanged(GamePhase gamePhase)
+        {
+            PhaseType phaseType = GamePhaseConversor(gamePhase);
+            if(phaseType == PhaseType.TRANSITION)
+            {
+                ActiveAll(false);
+            }
+            else
+            {
+                ActiveAll(true);
+                progress.gameObject.SetActive(phaseType != PhaseType.SECOND);
+                countdown.gameObject.SetActive(phaseType == PhaseType.SECOND);
+            }
+
+            progress.SetPhase(phaseType);
+        }
+
+        public void OnActionUsed(ActionType actionType, float cooldown)
+        {
+            m_actionControllers[(int)actionType].Use(cooldown);
+        }
+
+        public void OnBossProgress(float percent)
+        {
+            progress.SetPercent(percent);
+        }
+
+        public void OnCountdownSetted(float time)
+        {
+            countdown.Go(time);
+        }
+
+        public void OnSetSetted(SetType setType)
+        {
+            throw new System.NotImplementedException("Not Implemented");
+        }
+
+        private PhaseType GamePhaseConversor(GamePhase phase)
         {
             PhaseType ret = PhaseType.TRANSITION;
             switch (phase)
@@ -136,45 +146,7 @@ namespace Run4YourLife.UI
                     }
                     break;
             }
-            OnPhaseSetted(ret);
-        }
-
-        public void OnActionUsed(ActionType actionType, float time)
-        {
-            imageUpdaters[(int)actionType].Use(time);
-            scaleTicksBottom[(int)actionType].Tick();
-            scaleTicksTop[(int)actionType].Tick();
-        }
-
-        public void OnSetSetted(SetType setType)
-        {
-            throw new System.NotImplementedException("Not Implemented");
-        }
-
-        public void OnPhaseSetted(PhaseType phaseType)
-        {
-            if(phaseType == PhaseType.TRANSITION)
-            {
-                ActiveAll(false);
-            }
-            else
-            {
-                ActiveAll(true);
-                progress.gameObject.SetActive(phaseType != PhaseType.SECOND);
-                countdown.gameObject.SetActive(phaseType == PhaseType.SECOND);
-            }
-
-            progress.SetPhase(phaseType);
-        }
-
-        public void OnBossProgress(float percent)
-        {
-            progress.SetPercent(percent);
-        }
-
-        public void OnCountdownSetted(float time)
-        {
-            countdown.Go(time);
+            return ret;
         }
 
         private void ActiveAll(bool active)
@@ -186,3 +158,4 @@ namespace Run4YourLife.UI
         }
     }
 }
+
