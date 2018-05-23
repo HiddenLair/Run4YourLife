de2b42b390862304423ed80d28a4198775b48b5c
Pere Viader <pere.viader22@gmail.com>
2018-05-23 23:38:59 +0200

    Changed how falling platform detect player collision

---
+++ Run4YourLife/Assets/Code/Generic/Utils/Movement/MoveWithGravity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MoveWithGravity : MonoBehaviour {
+
+    [SerializeField]
+    private float m_gravity;
+
+    public bool IsGravityEnabled { get; set; }
+
+    private float m_yVelocity;
+
+    private void Update()
+    {
+        m_yVelocity += m_gravity * Time.deltaTime;
+        Vector3 position = transform.position;
+        position.y += m_yVelocity * Time.deltaTime;
+        transform.position = position;
+    }
+}
---
+++ Run4YourLife/Assets/Code/Interactables/FallingPlatform/FallingPlatformNotifier.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Run4YourLife;
+
+namespace Run4YourLife.Interactables
+{
+    public class FallingPlatformNotifier : MonoBehaviour {
+
+        private FallingPlatformController m_fallingPlatformController;
+
+        private void Awake()
+        {
+            m_fallingPlatformController = transform.parent.GetComponent<FallingPlatformController>();
+            Debug.Assert(m_fallingPlatformController);
+        }
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            if (collider.CompareTag(Tags.Runner))
+            {
+                m_fallingPlatformController.OnRunnerWalkedOnTop();
+            }
+        }
+    }
+}
