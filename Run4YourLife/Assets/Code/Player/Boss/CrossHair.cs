7ce45d69cce59a200bf67e528ad8ed7c8b3e595b
gerardpf2 <gerard.contacte@gmail.com>
2018-05-26 13:40:06 +0200

    Solved crosshair trigger enter / exit problems

--- Run4YourLife/Assets/Code/Player/Boss/CrossHair.cs
+++ Run4YourLife/Assets/Code/Player/Boss/CrossHair.cs
@@ -1,14 +1,11 @@
 ﻿using UnityEngine;
 
-using Run4YourLife.UI;
-
 namespace Run4YourLife.Player
 {
     public class CrossHair : MonoBehaviour
     {
-        private Collider collidingObject = null;
-
         private bool m_isOperative = true;
+        private Collider collidingObject = null;
 
         public bool IsOperative { get { return m_isOperative; } set { m_isOperative = value; } }
         
@@ -20,15 +17,28 @@ namespace Run4YourLife.Player
 
         private void OnTriggerExit(Collider other)
         {
-            m_isOperative = true;
-            collidingObject = null;
+            /*
+
+            If 2 objects are in contact (extreme case: the 2nd one is partially inside of the 1st one) it can happen that
+            the OnTriggerEnter of the 2nd one triggers before the OnTriggerExit of the 1st one
+            Update m_isOperative only if we are exiting the previously registered collider
+            If that is not the case, then this will be managed inside Update
+
+            */
+
+            if(collidingObject == other)
+            {
+                m_isOperative = true;
+                collidingObject = null;
+            }
         }
 
         private void Update()
         {
-            if(!m_isOperative && !collidingObject)
+            if(!m_isOperative && (!collidingObject || !collidingObject.gameObject.activeInHierarchy))
             {
                 m_isOperative = true;
+                collidingObject = null;
             }
         }
     }
