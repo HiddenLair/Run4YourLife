0f02a7aef384f757297c07ec0eb2c9e6fa9e0dac
Moaif <32565030+Moaif@users.noreply.github.com>
2018-05-28 15:19:15 +0200

    Added loaded  shoot particles

--- Run4YourLife/Assets/Code/Player/Boss/Shoot3.cs
+++ Run4YourLife/Assets/Code/Player/Boss/Shoot3.cs
@@ -15,16 +15,22 @@ namespace Run4YourLife.Player
         #region Inspector
 
         [SerializeField]
-        private GameObject laser;
+        private GameObject dangerIndicator;
 
         [SerializeField]
-        private GameObject dangerIndicator;
+        private Transform dangerRotator;
 
         [SerializeField]
         private float maxLaserDistance = 50.0f;
 
         [SerializeField]
         private float laserWidth = 1.0f;
+
+        [SerializeField]
+        private FXReceiver loadShootReceiver;
+
+        [SerializeField]
+        private GameObject loadedShootParticles;
 
         #endregion
 
@@ -36,12 +42,21 @@ namespace Run4YourLife.Player
             newScale.x = newScale.z = laserWidth;
             newScale.y = maxLaserDistance;
             dangerIndicator.transform.localScale = newScale;
-            Vector3 newPos = new Vector3(newScale.y, 0, 0);
-            dangerIndicator.transform.localPosition = newPos;
+            dangerIndicator.transform.position = loadShootReceiver.transform.position;
+            Vector3 newLocalPos = dangerIndicator.transform.localPosition;
+            newLocalPos.x += newScale.y;
+            dangerIndicator.transform.localPosition = newLocalPos;
+
         }
 
         public override void ShootByAnim()
         {
+            Vector3 director = (crossHairControl.Position - loadShootReceiver.transform.position).normalized;
+            Quaternion rotation = Quaternion.FromToRotation(Vector3.right, director);
+
+            dangerRotator.localRotation = rotation;
+
+            loadShootReceiver.PlayFx();
             animator.SetTrigger("Shoot");
             AnimationPlayOnTimeManager.Instance.PlayOnNextAnimation(animator, "ChargeShoot", timeToShootFromAnim, () => Shoot());
             AnimationPlayOnTimeManager.Instance.PlayOnNextAnimation(animator, "ChargeShoot", 0.0f, () => StopIndicator());
@@ -55,10 +70,13 @@ namespace Run4YourLife.Player
 
         void Shoot()
         {
-            RaycastHit[] targetLocation;
+            Vector3 director = (crossHairControl.Position - loadShootReceiver.transform.position).normalized;
+            Quaternion rotation = Quaternion.FromToRotation(Vector3.right,director);
 
-            laser.SetActive(true);
-            targetLocation = Physics.SphereCastAll(shootInitZone.position, laserWidth, shootInitZone.right, maxLaserDistance, Layers.Runner | Layers.Trap);
+            RaycastHit[] targetLocation;
+            FXManager.Instance.InstantiateFromValues(loadShootReceiver.transform.position, rotation, loadedShootParticles,loadShootReceiver.transform);
+
+            targetLocation = Physics.SphereCastAll(loadShootReceiver.transform.position, laserWidth, director, maxLaserDistance, Layers.Runner | Layers.Trap);
             foreach (RaycastHit r in targetLocation)
             {
                 ExecuteEvents.Execute<ICharacterEvents>(r.transform.gameObject, null, (x, y) => x.Kill());
