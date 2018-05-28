807916e511dd3f362ba9afb7f8a4adf80aaacfc9
Pere Viader <pere.viader22@gmail.com>
2018-05-28 11:57:07 +0200

    Added gamescene load from main menu in debug builds. Press F1

--- Run4YourLife/Assets/Code/Debug/DebugEventTriggerer.cs
+++ Run4YourLife/Assets/Code/Debug/DebugEventTriggerer.cs
@@ -5,6 +5,11 @@ public class DebugEventTriggerer : MonoBehaviour {
     public UnityEvent events;
     public KeyCode key;
 
+    private void Awake()
+    {
+        enabled = Debug.isDebugBuild;
+    }
+
     public void Update()
     {
         if(Input.GetKeyDown(key))
