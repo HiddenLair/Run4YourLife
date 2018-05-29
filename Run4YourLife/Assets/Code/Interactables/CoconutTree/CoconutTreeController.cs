135f8ffe0365fb895e2b2a2369e0833ca38a9a79
Pere Viader <pere.viader22@gmail.com>
2018-05-29 12:06:33 +0200

    Refactored coconut tree #295

---
+++ Run4YourLife/Assets/Code/Interactables/CoconutTree/CoconutTreeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Run4YourLife.Utils;
+
+public class CoconutTreeController : MonoBehaviour {
+
+    [SerializeField]
+    private float m_timeToReappear;
+
+    private CoconutController m_coconutController;
+
+    private void Awake()
+    {
+        m_coconutController = GetComponentInChildren<CoconutController>();
+        Debug.Assert(m_coconutController != null);
+    }
+
+    public void OnRunnerTriggeredCoconutFall()
+    {
+        if(m_coconutController.Aviable)
+        {
+            m_coconutController.Fall();
+            StartCoroutine(YieldHelper.WaitForSeconds(() => ResetCoconut(), m_timeToReappear));
+        }
+    }
+
+    public void ResetCoconut()
+    {
+        m_coconutController.Reset();
+    }
+}
---
+++ Run4YourLife/Assets/Code/Interactables/CoconutTree/CoconutTreeNotifier.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoconutTreeNotifier : MonoBehaviour {
+
+    private CoconutTreeController m_coconutTreeController;
+
+    private void Awake()
+    {
+        m_coconutTreeController = transform.parent.GetComponent<CoconutTreeController>();
+        Debug.Assert(m_coconutTreeController != null);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_coconutTreeController.OnRunnerTriggeredCoconutFall();
+    }
+}
