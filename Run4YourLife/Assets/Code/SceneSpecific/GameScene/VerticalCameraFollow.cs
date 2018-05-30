a0149f190e3d1afafca8b479688ab89ec5bfad39
Moaif <32565030+Moaif@users.noreply.github.com>
2018-05-30 15:41:25 +0200

    Added vertical kill

---
+++ Run4YourLife/Assets/Code/SceneSpecific/GameScene/VerticalCameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class VerticalCameraFollow : MonoBehaviour {
+
+
+    #region Inspector
+    [SerializeField]
+    private float offset;
+    #endregion
+    #region variables
+    private Camera camera;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPos = transform.position;
+        newPos.y  = camera.ScreenToWorldPoint(new Vector3(0, 0, Math.Abs(camera.transform.position.z - transform.position.z))).y;
+        newPos.y -= offset;
+        transform.position = newPos;
+    }
+}
