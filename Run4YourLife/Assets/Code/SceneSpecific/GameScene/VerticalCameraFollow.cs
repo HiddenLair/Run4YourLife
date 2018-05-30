4c833efde3abfb40d7a1ec86e27997c83bb6e481
Pere Viader <pere.viader22@gmail.com>
2018-05-30 16:03:38 +0200

    Fixed bug where changing phase would throw index out of range  and refactored collider kill

--- Run4YourLife/Assets/Code/SceneSpecific/GameScene/VerticalCameraFollow.cs
+++ Run4YourLife/Assets/Code/SceneSpecific/GameScene/VerticalCameraFollow.cs
@@ -1,29 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
+﻿using UnityEngine;
 
-public class VerticalCameraFollow : MonoBehaviour {
+using Run4YourLife.GameManagement;
 
+namespace Run4YourLife.SceneSpecific.GameScene
+{
+    public class VerticalCameraFollow : MonoBehaviour {
 
-    #region Inspector
-    [SerializeField]
-    private float offset;
-    #endregion
-    #region variables
-    private Camera camera;
-    #endregion
+        [SerializeField]
+        private float m_offset;
 
-    // Use this for initialization
-    void Start () {
-        camera = Camera.main;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Vector3 newPos = transform.position;
-        newPos.y  = camera.ScreenToWorldPoint(new Vector3(0, 0, Math.Abs(camera.transform.position.z - transform.position.z))).y;
-        newPos.y -= offset;
-        transform.position = newPos;
+        private Camera m_mainCamera;
+
+        void Start () {
+            m_mainCamera = CameraManager.Instance.MainCamera;
+        }
+        
+        // Update is called once per frame
+        void Update () {
+            float bottomYCameraPosition = m_mainCamera.ScreenToWorldPoint(new Vector3(0, 0, Mathf.Abs(m_mainCamera.transform.position.z - transform.position.z))).y;
+            
+            Vector3 newPos = transform.position;
+            newPos.y  = bottomYCameraPosition + m_offset;
+            
+            transform.position = newPos;
+        }
     }
+ 
 }
