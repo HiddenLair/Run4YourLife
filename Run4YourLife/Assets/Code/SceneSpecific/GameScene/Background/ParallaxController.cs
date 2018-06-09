3b85cd5f1fc31c5536679b8624f2d63eaad148c8
gerardpf2 <gerard.contacte@gmail.com>
2018-06-09 19:16:21 +0200

    Updated parallax system

---
+++ Run4YourLife/Assets/Code/SceneSpecific/GameScene/Background/ParallaxController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+using Run4YourLife.GameManagement;
+
+[RequireComponent(typeof(Renderer))]
+public class ParallaxController : MonoBehaviour
+{
+    /*
+
+    Use of texture offset for h movement
+    Use of transform position for v movement
+
+    */
+
+    [SerializeField]
+    private float hSpeed;
+
+    [SerializeField]
+    private float vSpeed;
+
+    private new Renderer renderer;
+
+    private Material material;
+    private Material sharedMaterial;
+
+    private float initialX;
+    private float initialY;
+    private float cameraInitialX;
+    private float cameraInitialY;
+    private Camera storedMainCamera;
+
+    void Awake()
+    {
+        renderer = GetComponent<Renderer>();
+
+        material = renderer.material;
+        sharedMaterial = renderer.sharedMaterial;
+
+        initialX = transform.position.x;
+        initialY = transform.position.y;
+    }
+
+    void Update()
+    {
+        UpdateStoredCamera();
+
+        UpdateHMovement();
+        UpdateVMovement();
+    }
+
+    void OnDestroy()
+    {
+        renderer.sharedMaterial = sharedMaterial;
+    }
+
+    private void UpdateStoredCamera()
+    {
+        Camera camera = CameraManager.Instance.MainCamera;
+
+        if(storedMainCamera != camera)
+        {
+            storedMainCamera = camera;
+
+            cameraInitialX = camera.transform.position.x;
+            cameraInitialY = camera.transform.position.y;
+        }
+    }
+
+    private void UpdateHMovement()
+    {
+        float cameraAmountX = CameraManager.Instance.MainCamera.transform.position.x - cameraInitialX;
+        float currentX = hSpeed * cameraAmountX;
+
+        material.mainTextureOffset = Vector2.right * (currentX - initialX);
+    }
+
+    private void UpdateVMovement()
+    {
+        float cameraAmountY = CameraManager.Instance.MainCamera.transform.position.y - cameraInitialY;
+        float currentY = vSpeed * cameraAmountY;
+
+        Vector3 newPosition = transform.position;
+        newPosition.y = initialY - currentY;
+        transform.position = newPosition;
+    }
+}
