3ab587e5c24ca884de2f5368ed12afc7935a643b
ZelleryonDesktop <xavibravo.guillen@gmail.com>
2018-06-17 20:00:33 +0200

    Gems now teleport within positions with fancy sounds and visuals!

--- Run4YourLife/Assets/Code/Generic/Utils/Movement/MoveThroughPoints.cs
+++ Run4YourLife/Assets/Code/Generic/Utils/Movement/MoveThroughPoints.cs
@@ -10,39 +10,61 @@ public class MoveThroughPoints : MonoBehaviour {
     [SerializeField]
     private List<Transform> pointsToFollow;
 
+    [SerializeField]
+    private float secondsOnPosition = 2.5f;
+
+    [SerializeField]
+    private float secondsTeleporting = 2.5f;
+
+    private float timeOnPoint = 0;
+
+    private Renderer rend;
+
     private Random randomGenerator = new Random();
 
-    private Vector3 originalPosition;
-    private Vector3 targetPosition;
-
-    private float timeWhenRecalculating;
-    private float pathLength;
+    private Vector3 previousPosition = new Vector3(0, 0, 0);
+    private Vector3 targetPosition = new Vector3(0, 0, 0);
 
     private void Start()
     {
-        GetNewTarget();
+        previousPosition = new Vector3(0, 0, 0);
+        rend = GetComponent<Renderer>();
+        timeOnPoint = Time.time;
     }
 
     void Update ()
     {
-		if(gameObject.transform.position != targetPosition)
+		if(Time.time - timeOnPoint > secondsOnPosition + secondsTeleporting)
         {
-            float fracJourney = ((Time.time - timeWhenRecalculating) * m_gemSpeed) / pathLength;
-            gameObject.transform.position = Vector3.Lerp(originalPosition, targetPosition, fracJourney);
-        }
-        else
-        {
-            GetNewTarget();
+            TeleportToNewPosition();
         }
 	}
 
-    private void GetNewTarget()
+    private void TeleportToNewPosition()
     {
-        timeWhenRecalculating = Time.time;
-        originalPosition = gameObject.transform.position;
+        previousPosition = targetPosition;
         targetPosition = pointsToFollow[Random.Range(0, pointsToFollow.Count - 1)].position;
 
-        pathLength = Vector3.Distance(originalPosition, targetPosition);
-        //In case that the number is the same than before, this iteration will be repeated so... no problem!
+        while (previousPosition == targetPosition)
+        {
+            targetPosition = pointsToFollow[Random.Range(0, pointsToFollow.Count - 1)].position;
+        }
+
+        StartCoroutine(TeleportingCoroutine(secondsTeleporting));
+
+        gameObject.transform.position = targetPosition;
+
+        timeOnPoint = Time.time;
+    }
+
+    private IEnumerator TeleportingCoroutine(float seconds)
+    {
+        //Fancy disappear
+        rend.enabled = false;
+
+        yield return new WaitForSeconds(seconds);
+
+        //Fancy appear
+        rend.enabled = true;
     }
 }
