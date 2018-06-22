accd63d0ff25964ec3965aae3441484b4f634a0c
gerardpf2 <gerard.contacte@gmail.com>
2018-06-22 16:39:34 +0200

    Global and cinematic updated names

--- Run4YourLife/Assets/Code/SceneSpecific/Global/TransitionToCinematicGloboalSceneController.cs
+++ Run4YourLife/Assets/Code/SceneSpecific/Global/TransitionToCinematicGloboalSceneController.cs
@@ -1,18 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using Run4YourLife.SceneManagement;
-
-using Run4YourLife.Utils;
 
 namespace Run4YourLife.SceneSpecific.Global
 {
-    public class TransitionToMainMenuGloboalSceneController : MonoBehaviour
+    public class TransitionToCinematicGloboalSceneController : MonoBehaviour
     {
         [SerializeField]
         private SceneTransitionRequest m_sceneTransitionRequest;
 
         private void Start()
         {
-            StartCoroutine(YieldHelper.WaitForSeconds(()=> m_sceneTransitionRequest.Execute(), 17.0f));
+            if(SceneManager.sceneCount == 1)
+            {
+                m_sceneTransitionRequest.Execute();
+            }
         }
     }
 }
