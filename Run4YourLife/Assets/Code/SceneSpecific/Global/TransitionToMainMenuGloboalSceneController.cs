96dde8b14e116ef5720042e411ec2164be76a58e
gerardpf2 <gerard.contacte@gmail.com>
2018-06-23 18:26:24 +0200

    Cinematic can be skipped

--- Run4YourLife/Assets/Code/SceneSpecific/Global/TransitionToMainMenuGloboalSceneController.cs
+++ Run4YourLife/Assets/Code/SceneSpecific/Global/TransitionToMainMenuGloboalSceneController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 using Run4YourLife.SceneManagement;
 
 using Run4YourLife.Utils;
@@ -10,9 +11,34 @@ namespace Run4YourLife.SceneSpecific.Global
         [SerializeField]
         private SceneTransitionRequest m_sceneTransitionRequest;
 
+        [SerializeField]
+        private GameObject uiSkipInfo;
+
+        [SerializeField]
+        private float uiSkipInfoTime0 = 1.0f;
+
+        [SerializeField]
+        private float uiSkipInfoTime1 = 3.5f;
+
         private void Start()
         {
+            Debug.Assert(uiSkipInfoTime0 <= uiSkipInfoTime1 && uiSkipInfoTime0 >= 0.0f, "TransitionToMainMenuGloboalSceneController: Specified times are not correct!");
+
+            StartCoroutine(UISkipInfoEnabler());
             StartCoroutine(YieldHelper.WaitForSeconds(()=> m_sceneTransitionRequest.Execute(), 17.0f));
+        }
+
+        private IEnumerator UISkipInfoEnabler()
+        {
+            uiSkipInfo.SetActive(false);
+
+            yield return new WaitForSeconds(uiSkipInfoTime0);
+
+            uiSkipInfo.SetActive(true);
+
+            yield return new WaitForSeconds(uiSkipInfoTime1);
+
+            uiSkipInfo.SetActive(false);
         }
     }
 }
