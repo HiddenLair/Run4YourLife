1a878d725052ec433daef36e6135768796dad6fa
gerardpf2 <gerard.contacte@gmail.com>
2018-06-23 17:24:06 +0200

    Added OnButtonPressedControlScheme in order to simplify scene global OnButtonPressed events (like B in OptionsMenu) and updated OptionsMenu

---
+++ Run4YourLife/Assets/Code/Input/ControlSchemes/OnButtonPressedControlScheme.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Run4YourLife.InputManagement
+{
+    public class OnButtonPressedControlScheme : ControlScheme
+    {
+        [SerializeField]
+        private string buttonId;
+
+        [SerializeField]
+        private UnityEvent onButtonPressed;
+
+        public InputAction InputAction { get; private set; }
+
+        void Awake()
+        {
+            InputActions.Add(InputAction = new InputAction(new InputSource(GetButton()), "OneButtonControlScheme::InputActions"));
+        }
+
+        void Start()
+        {
+            InputDevice = InputDeviceManager.Instance.DefaultInputDevice;
+            Active = true;
+        }
+
+        void Update()
+        {
+            if(InputAction.Started())
+            {
+                onButtonPressed.Invoke();
+            }
+        }
+
+        private Button GetButton()
+        {
+            Button button = null;
+
+            foreach(Button currentButton in Button.BUTTONS)
+            {
+                if(currentButton.ID == buttonId)
+                {
+                    button = currentButton;
+                    break;
+                }
+            }
+
+            Debug.Assert(button != null, "OnButtonPressedControlScheme: buttonId does not exist!");
+
+            return button;
+        }
+    }
+}
--- Run4YourLife/Assets/Code/SceneSpecific/Options Menu/OptionsMenuManager.cs
+++ Run4YourLife/Assets/Code/SceneSpecific/Options Menu/OptionsMenuManager.cs
@@ -1,29 +1,17 @@
 ﻿using UnityEngine;
 
-using Run4YourLife.SceneManagement;
 using UnityEngine.EventSystems;
-using Run4YourLife.InputManagement;
+using Run4YourLife.SceneManagement;
 
 namespace Run4YourLife.SceneSpecific.OptionsMenu
 {
-    [RequireComponent(typeof(OptionsMenuControlScheme))]
     public class OptionsMenuManager : MonoBehaviour
     {
         [SerializeField]
         private SceneTransitionRequest m_unloadOptionsMenu;
 
-        private OptionsMenuControlScheme m_optionsMenuControlScheme;
-
-        private void Awake()
-        {
-            m_optionsMenuControlScheme = GetComponent<OptionsMenuControlScheme>();
-        }
-
         private void Start()
         {
-            m_optionsMenuControlScheme.InputDevice = InputDeviceManager.Instance.DefaultInputDevice;
-            m_optionsMenuControlScheme.Active = true;
-
             GameObject firstSelectedGameObject = EventSystem.current.firstSelectedGameObject;
             if(firstSelectedGameObject != null)
             {
@@ -32,14 +20,6 @@ namespace Run4YourLife.SceneSpecific.OptionsMenu
                 {
                     firstSelected.OnSelect(null);
                 }
-            }
-        }
-
-        private void Update()
-        {
-            if(m_optionsMenuControlScheme.Exit.Started())
-            {
-                m_unloadOptionsMenu.Execute();
             }
         }
     }
