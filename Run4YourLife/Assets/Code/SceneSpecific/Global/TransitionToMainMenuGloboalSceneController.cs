255e8a0088293a0df1e3a63c1904b15856c4468d
gerardpf2 <gerard.contacte@gmail.com>
2018-06-24 13:52:18 +0200

    Cinematic and CharacterSelection minor updates

--- Run4YourLife/Assets/Code/SceneSpecific/Global/TransitionToMainMenuGloboalSceneController.cs
+++ Run4YourLife/Assets/Code/SceneSpecific/Global/TransitionToMainMenuGloboalSceneController.cs
@@ -12,7 +12,7 @@ namespace Run4YourLife.SceneSpecific.Global
         private SceneTransitionRequest m_sceneTransitionRequest;
 
         [SerializeField]
-        private GameObject uiSkipInfo;
+        private CanvasGroup uiSkipInfo;
 
         [SerializeField]
         private float uiSkipInfoTime0 = 1.0f;
@@ -20,25 +20,45 @@ namespace Run4YourLife.SceneSpecific.Global
         [SerializeField]
         private float uiSkipInfoTime1 = 3.5f;
 
+        [SerializeField]
+        private float uiSkipInfoFadeTime = 0.5f;
+
         private void Start()
         {
             Debug.Assert(uiSkipInfoTime0 <= uiSkipInfoTime1 && uiSkipInfoTime0 >= 0.0f, "TransitionToMainMenuGloboalSceneController: Specified times are not correct!");
 
-            StartCoroutine(UISkipInfoEnabler());
+            StartCoroutine(UISkipInfoUpdater());
             StartCoroutine(YieldHelper.WaitForSeconds(()=> m_sceneTransitionRequest.Execute(), 17.0f));
         }
 
-        private IEnumerator UISkipInfoEnabler()
+        private IEnumerator UISkipInfoUpdater()
         {
-            uiSkipInfo.SetActive(false);
+            uiSkipInfo.alpha = 0.0f;
 
             yield return new WaitForSeconds(uiSkipInfoTime0);
 
-            uiSkipInfo.SetActive(true);
+            StartCoroutine(UIAlphaUpdater(0.0f, 1.0f, uiSkipInfoFadeTime));
 
             yield return new WaitForSeconds(uiSkipInfoTime1);
 
-            uiSkipInfo.SetActive(false);
+            StartCoroutine(UIAlphaUpdater(1.0f, 0.0f, uiSkipInfoFadeTime));
+        }
+
+        private IEnumerator UIAlphaUpdater(float alpha0, float alpha1, float timeS)
+        {
+            float iTime = Time.time;
+            float cTime = iTime;
+            float eTime = iTime + timeS;
+
+            while(cTime < eTime)
+            {
+                uiSkipInfo.alpha = Mathf.Clamp01(Mathf.Lerp(alpha0, alpha1, (cTime - iTime) / timeS));
+
+                yield return null;
+                cTime = Time.time;
+            }
+
+            uiSkipInfo.alpha = alpha1;
         }
     }
 }
