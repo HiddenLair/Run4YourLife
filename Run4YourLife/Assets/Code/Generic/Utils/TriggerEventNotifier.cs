6bab58a136fe0725e23e7c61bd34b1ee92f302ed
Moaif <32565030+Moaif@users.noreply.github.com>
2018-06-26 17:09:44 +0200

    Working on second Transition

--- Run4YourLife/Assets/Code/Generic/Utils/TriggerEventNotifier.cs
+++ Run4YourLife/Assets/Code/Generic/Utils/TriggerEventNotifier.cs
@@ -5,17 +5,39 @@ namespace Run4YourLife.Utils
 {
     public class TriggerEventNotifier : MonoBehaviour
     {
+        [SerializeField]
+        private LayerMask layersToTest;
+        [SerializeField]
+        private string tagToTest;
         public UnityEvent triggerEnter;
         public UnityEvent triggerExit;
 
         private void OnTriggerEnter(Collider other)
         {
-            triggerEnter.Invoke();
+            Check(other.gameObject,triggerEnter);
         }
 
         private void OnTriggerExit(Collider other)
         {
-            triggerExit.Invoke();
+            Check(other.gameObject,triggerExit);
+        }
+
+        private void Check(GameObject g, UnityEvent e)
+        {
+            if (((1 << g.layer) & layersToTest) != 0)
+            {
+                if(tagToTest != "")
+                {
+                    if (g.CompareTag(tagToTest))
+                    {
+                        e.Invoke();
+                    }
+                }
+                else
+                {
+                    e.Invoke();
+                }
+            }
         }
     }
 }
--- Run4YourLife/Assets/Code/SceneSpecific/GameScene/GamePhaseManager/TransitionToBossFightPhaseManager.cs
+++ Run4YourLife/Assets/Code/SceneSpecific/GameScene/GamePhaseManager/TransitionToBossFightPhaseManager.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
 using Cinemachine;
 
 namespace Run4YourLife.GameManagement
@@ -10,11 +11,95 @@ namespace Run4YourLife.GameManagement
     {
         public override GamePhase GamePhase { get { return GamePhase.TransitionToBossFight; } }
 
+        [SerializeField]
+        private PlayableDirector m_startingCutscene;
+
+        private Coroutine m_startPhaseCoroutine;
+        private TimelineAsset timelineAsset;
+
         public override void StartPhase()
-        {                     
+        {
+            m_startPhaseCoroutine = StartCoroutine(StartPhaseCoroutine());
+        }
+
+        private IEnumerator StartPhaseCoroutine()
+        {
+
+            List<GameObject> runners = GameplayPlayerManager.Instance.Runners;
+            foreach (GameObject runner in runners)
+            {
+                DeactivateScripts(runner);
+            }
+            GameObject boss = GameplayPlayerManager.Instance.Boss;
+            DeactivateScripts(boss);
+
+            BindTimelineTracks(runners, boss);
+            m_startingCutscene.RebuildGraph();
+            m_startingCutscene.Play();
+            yield return new WaitUntil(() => m_startingCutscene.state != PlayState.Playing); // wait until cutscene has completed
+            Unbind();
+            foreach (GameObject runner in runners)
+            {
+                ActivateScripts(runner);
+            }
+            ActivateScripts(boss);
             GameManager.Instance.ChangeGamePhase(GamePhase.BossFight);
         }
 
+        private void DeactivateScripts(GameObject g)
+        {
+            foreach (MonoBehaviour mono in g.GetComponents<MonoBehaviour>())
+            {
+                mono.enabled = false;
+            }
+        }
+
+        private void ActivateScripts(GameObject g)
+        {
+            foreach (MonoBehaviour mono in g.GetComponents<MonoBehaviour>())
+            {
+                mono.enabled = true;
+            }
+            Animator anim = g.GetComponent<Animator>();
+            Avatar temp = anim.avatar;
+            anim.avatar = null;
+            anim.avatar = temp;
+        }
+
+        private void BindTimelineTracks(List<GameObject> runners, GameObject boss)
+        {
+            timelineAsset = (TimelineAsset)m_startingCutscene.playableAsset;
+            var outputs = timelineAsset.outputs;
+            foreach (var itm in outputs)
+            {              
+                if (itm.streamName.Contains("Player1") && runners.Count > 0)
+                {
+                    m_startingCutscene.SetGenericBinding(itm.sourceObject, runners[0]);
+                }
+                else if (itm.streamName.Contains("Player2") && runners.Count > 1)
+                {
+                    m_startingCutscene.SetGenericBinding(itm.sourceObject, runners[1]);
+                }
+                else if (itm.streamName.Contains("Player3") && runners.Count > 2)
+                {
+                    m_startingCutscene.SetGenericBinding(itm.sourceObject, runners[2]);
+                }
+                else if (itm.streamName.Contains("Boss"))
+                {
+                    m_startingCutscene.SetGenericBinding(itm.sourceObject, boss);
+                }
+            }
+        }
+
+        private void Unbind()
+        {
+            timelineAsset = (TimelineAsset)m_startingCutscene.playableAsset;
+            var outputs = timelineAsset.outputs;
+            foreach (var itm in outputs)
+            {
+                m_startingCutscene.SetGenericBinding(itm.sourceObject, null);
+            }
+        }
         public override void EndPhase()
         {
             
