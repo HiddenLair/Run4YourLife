610dd5a194260fe0c57cf5f0a886a47fadfe3dbf
Pere Viader <pere.viader22@gmail.com>
2018-07-03 23:02:47 +0200

    Refactored and fixed falling totem #448

---
+++ Run4YourLife/Assets/Code/Interactables/FallingTotem/FallingTotemController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using UnityEngine;
+using Run4YourLife.GameManagement;
+using Run4YourLife.GameManagement.AudioManagement;
+
+
+namespace Run4YourLife.Interactables
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class FallingTotemController : MonoBehaviour
+    {
+        [SerializeField]
+        private TrembleConfig m_trembleConfig;
+
+        [SerializeField]
+        private AudioClip m_groundHitSound;
+
+        [SerializeField]
+        private float m_initialFallSpeed;
+
+        [SerializeField]
+        private float m_gravity;
+
+        [SerializeField]
+        private float m_endRotationX;
+
+        [SerializeField]
+        private Collider m_fallingTotemTrigger;
+
+        [SerializeField]
+        private Rigidbody m_rigidbody;
+
+        [SerializeField]
+        private FXReceiver m_dustParticles;
+
+        private float m_rotationSpeed;
+
+        private void Reset()
+        {
+            m_fallingTotemTrigger = transform.Find("TriggerFall").GetComponent<Collider>();
+            m_rigidbody = GetComponent<Rigidbody>();
+            m_dustParticles = transform.Find("Dust").GetComponent<FXReceiver>();
+        }
+
+        public void OnRunnerTriggeredTotemFall()
+        {
+            StartCoroutine(ExecuteTotemFall());
+        }
+
+        public void ResetTotem()
+        {
+            StopAllCoroutines();
+            m_fallingTotemTrigger.enabled = true;
+            transform.rotation = Quaternion.identity;
+            //m_rigidbody.rotation = Quaternion.identity; // todo why does this not work?
+        }
+
+        private IEnumerator ExecuteTotemFall()
+        {
+            m_fallingTotemTrigger.enabled = false;
+            m_rotationSpeed = m_initialFallSpeed;
+
+            Quaternion startingRotation = m_rigidbody.rotation;
+            Quaternion desiredRotation = m_rigidbody.rotation * Quaternion.Euler(m_endRotationX,0,0);
+
+            float t = 0f;
+
+            while (t < 1f)
+            {
+                Quaternion rotation = Quaternion.Lerp(startingRotation, desiredRotation, t);
+                m_rigidbody.MoveRotation(rotation);
+                yield return null;
+                m_rotationSpeed += m_gravity * Time.deltaTime;
+                t += m_rotationSpeed * Time.deltaTime;
+            }
+
+            TrembleManager.Instance.Tremble(m_trembleConfig);
+            AudioManager.Instance.PlaySFX(m_groundHitSound);
+            m_dustParticles.PlayFx(false);
+        }
+
+    }
+}
---
+++ Run4YourLife/Assets/Code/Interactables/FallingTotem/FallingTotemTriggerNotifier.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Run4YourLife.Interactables
+{
+    public class FallingTotemTriggerNotifier : MonoBehaviour {
+
+        [SerializeField]
+        private FallingTotemController m_totemController;
+
+        private void Reset()
+        {
+            m_totemController = transform.parent.GetComponent<FallingTotemController>();
+            Debug.Assert(m_totemController != null);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(Tags.Runner))
+            {
+                m_totemController.OnRunnerTriggeredTotemFall();
+            }
+        }
+    }
+}
+
