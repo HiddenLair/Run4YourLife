4b6817ed7cc00c3e88843e5590f7ff0b6a88446d
Pere Viader <pere.viader22@gmail.com>
2018-07-04 21:50:37 +0200

    Adding sound to breakable wall #413

--- Run4YourLife/Assets/Code/Interactables/BreakableWall/BreakableWallController.cs
+++ Run4YourLife/Assets/Code/Interactables/BreakableWall/BreakableWallController.cs
@@ -2,25 +2,25 @@
 using System.Collections.Generic;
 
 using UnityEngine;
-
-using Run4YourLife.Player;
 using UnityEngine.Events;
+
+using Run4YourLife.GameManagement.AudioManagement;
 
 namespace Run4YourLife.Interactables
 {
     public class BreakableWallController : MonoBehaviour, IRunnerDashBreakable
     {
         [SerializeField]
-        private FXReceiver receiver;
+        private FXReceiver m_receiver;
 
         [SerializeField]
-        private UnityEvent m_onBrokenByDash;
+        private AudioClip m_destroyClip;
 
         public void Break()
         {
-            m_onBrokenByDash.Invoke();
             gameObject.SetActive(false);
-            receiver.PlayFx();
+            m_receiver.PlayFx();
+            AudioManager.Instance.PlaySFX(m_destroyClip);
         }
     }
 }
