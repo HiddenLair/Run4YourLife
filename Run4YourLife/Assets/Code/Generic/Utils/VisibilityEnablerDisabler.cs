d7bca8125d1885c2e4d06ebb332d4e203327412c
Pere Viader <pere.viader22@gmail.com>
2018-07-09 10:29:53 +0200

    Fixing launcher plant sound playing when not in screen #465

---
+++ Run4YourLife/Assets/Code/Generic/Utils/VisibilityEnablerDisabler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Run4YourLife.Utils
+{
+    public class VisibilityEnablerDisabler : MonoBehaviour {
+        
+        [SerializeField]
+        private MonoBehaviour m_monoBehaviour;
+
+        private void OnBecameInvisible()
+        {
+            m_monoBehaviour.enabled = false;
+        }
+
+        private void OnBecameVisible()
+        {
+            m_monoBehaviour.enabled = true;
+        }
+    }
+}
