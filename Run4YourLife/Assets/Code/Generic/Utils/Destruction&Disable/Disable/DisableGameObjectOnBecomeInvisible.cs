1e8d55751149efb85bef5565b0da3378769ba9e8
Pere Viader <pere.viader22@gmail.com>
2018-07-11 17:06:59 +0200

    Fixing boss skills prefabs scale, hierarchy, etc

---
+++ Run4YourLife/Assets/Code/Generic/Utils/Destruction&Disable/Disable/DisableGameObjectOnBecomeInvisible.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableGameObjectOnBecomeInvisible : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject m_gameObjectToDisable;
+
+    private void OnBecameInvisible()
+    {
+        m_gameObjectToDisable.SetActive(false);
+    }
+}
