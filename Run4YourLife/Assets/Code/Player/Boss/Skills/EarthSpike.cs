db720351a72a923a12348b36f5e2848350cf0f5f
Pere Viader <pere.viader22@gmail.com>
2018-07-15 17:26:51 +0200

    Implemented earth spike spawn when crosshair is inside rock. Removed legacy code. #468

--- Run4YourLife/Assets/Code/Player/Boss/Skills/EarthSpike.cs
+++ Run4YourLife/Assets/Code/Player/Boss/Skills/EarthSpike.cs
@@ -11,22 +11,20 @@ namespace Run4YourLife.Player
 {
     public class EarthSpike : SkillBase
     {
-
-        #region Inspector
-
         [SerializeField]
         private Vector3 m_spawnColliderBounds;
 
         [SerializeField]
-        private float delayHit;
+        [Tooltip("On screen duration of the earth spike warning particles")]
+        private float m_warningParticlesDuration;
 
         [SerializeField]
         [Tooltip("Time it takes the earth spike to fully grow")]
-        private float m_timeToGrow;
+        private float m_earthSpikeGrowthDuration;
 
         [SerializeField]
-        [Tooltip("Time it takes after the earth spike to be fully grown, to be broken")]
-        private float m_timeToBreak;
+        [Tooltip("After being fully grown, delay to be broken")]
+        private float m_breakEarthSpikeDelay;
 
         [SerializeField]
         private GameObject m_warningSpikeParticles;
@@ -40,64 +38,13 @@ namespace Run4YourLife.Player
         [SerializeField]
         private GameObject m_breakableWallPrefab;
 
-        #endregion
 
-        /*
-        public override bool CanBePlacedAt(ref SkillSpawnData skillSpawnData)
+        protected override void ResetState()
         {
-            Vector3 raycastPosition = skillSpawnData.position;
-            Collider[] colliders = Physics.OverlapBox(skillSpawnData.position, new Vector3(0.1f, 0.1f, 0.1f), Quaternion.identity, Layers.Stage, QueryTriggerInteraction.Ignore);
-            if (colliders.Length > 0)
-            {
-                Collider collider = LeftMostCollider(colliders);
-
-                //Especial Case
-                if (collider.CompareTag(Tags.Wall))
-                {
-                    SpawnUnder(collider, ref skillSpawnData);
-                    return true;
-                }
-
-                StageInfo info = FindStageInfoForCollider(collider);
-                Debug.Assert(info != null);
-
-                Camera mainCamera = CameraManager.Instance.MainCamera;
-                float bottomScreen = CameraConverter.ViewportToGamePlaneWorldPosition(mainCamera, new Vector2(0, 0)).y;
-
-                float minValue;
-                if (!info.GetMinValue(out minValue) || minValue < bottomScreen)
-                {
-                    Vector3 newPos = transform.position;
-                    newPos.y = info.GetTopValue();
-                    float topScreen = CameraConverter.ViewportToGamePlaneWorldPosition(mainCamera, new Vector2(1, 1)).y;
-                    if (newPos.y > topScreen)
-                    {
-                        return false;
-                    }
-                    skillSpawnData.position = newPos;
-                    skillSpawnData.parent = info.transform;
-                    return true;
-                }
-            }
-            
-
-
-            RaycastHit hitInfo;
-            if (Physics.Raycast(raycastPosition, Vector3.down, out hitInfo, Mathf.Infinity, Layers.Stage, QueryTriggerInteraction.Ignore))
-            {
-                if (hitInfo.collider.CompareTag(Tags.Wall))
-                {
-                    SpawnUnder(hitInfo.collider, ref skillSpawnData);
-                    return true;
-                }
-                skillSpawnData.position = hitInfo.point;
-                skillSpawnData.parent = hitInfo.transform;
-                return true;
-            }
-
-            return false;
+            StopAllCoroutines();
+            m_warningSpikeParticles.SetActive(false);
+            m_earthSpikeChild.SetActive(false);
         }
-        */
 
 
         public override bool CheckAndRepositionSkillSpawn(ref SkillSpawnData skillSpawnData)
@@ -107,7 +54,7 @@ namespace Run4YourLife.Player
             {
                 //Crosshair is in the air
                 RaycastHit validStageElement;
-                if (FindValidElementToSpawnAbove(skillSpawnData.position, out validStageElement)) // Cast ray below to find spawn position
+                if (FindValidStageElementToSpawnAbove(skillSpawnData.position, out validStageElement)) // Cast ray below to find spawn position
                 {
                     UpdateSkillSpawnDataSpawnLocation(validStageElement.collider, validStageElement.point, ref skillSpawnData);
                     return true;
@@ -115,10 +62,39 @@ namespace Run4YourLife.Player
             }
             else
             {
-                //TODO
-                Debug.Log("Earth spike inside stage element still not implemented");
+                Collider stageCollider = null;
+                Vector3 position = skillSpawnData.position;
+
+                if (SelectStageElementColliderToSpawnAbove(stageElements, ref stageCollider, ref position))
+                {
+                    UpdateSkillSpawnDataSpawnLocation(stageCollider, position, ref skillSpawnData);
+                }
+                return true;
             }
 
+            return false;
+        }
+
+        private bool SelectStageElementColliderToSpawnAbove(Collider[] colliders, ref Collider collider, ref Vector3 position)
+        {
+            StageInfo info = FindStageInfoForCollider(colliders[0]);
+            Debug.Assert(info != null);
+
+            Camera mainCamera = CameraManager.Instance.MainCamera;
+            float topScreenHeightPosition = CameraConverter.ViewportToGamePlaneWorldPosition(mainCamera, new Vector2(1, 1)).y;
+            float topStageElementHeightPosition = info.GetTopValue();
+
+            if (topStageElementHeightPosition < topScreenHeightPosition)
+            {
+                collider = colliders[0];
+                position = new Vector3()
+                {
+                    x = position.x,
+                    y = topStageElementHeightPosition,
+                    z = position.z
+                };
+                return true;
+            }
             return false;
         }
 
@@ -128,16 +104,21 @@ namespace Run4YourLife.Player
             return stageElements.Length > 0;
         }
 
-        private bool FindValidElementToSpawnAbove(Vector3 position, out RaycastHit validElement)
+        private bool FindValidStageElementToSpawnAbove(Vector3 position, out RaycastHit validElement)
         {
             RaycastHit[] hits = Physics.RaycastAll(position, Vector3.down, Mathf.Infinity, Layers.Stage, QueryTriggerInteraction.Ignore);
-            foreach (RaycastHit hit in hits)
+            if(hits.Length > 0)
             {
-                if (!hit.collider.CompareTag(Tags.Wall)) // we can't use walls as the ground for spawning it
+                int min = 0;
+                for (int i = 1; i < hits.Length; i++)
                 {
-                    validElement = hit;
-                    return true;
+                    if(hits[i].distance < hits[min].distance)
+                    {
+                        min = i;
+                    }
                 }
+                validElement = hits[min];
+                return true;
             }
 
             validElement = new RaycastHit();
@@ -145,8 +126,8 @@ namespace Run4YourLife.Player
         }
 
 
-        
-        
+
+
         /// Collider for the object
         /// Position to place the object. position.y should be equals to collider.bounds.max.y
         /// SkillSpawnData to update
@@ -157,7 +138,7 @@ namespace Run4YourLife.Player
             if (colliders.Length > 0)
             {
                 bool leftPentrating = colliders[0].bounds.center.x <= position.x;
-                if(leftPentrating) // Would penetrate a wall on its left
+                if (leftPentrating) // Would penetrate a wall on its left
                 {
                     position.x = colliders[0].bounds.max.x + halfBounds.x;
                 }
@@ -182,8 +163,7 @@ namespace Run4YourLife.Player
             skillSpawnData.parent = collider.transform;
         }
 
-
-        /*private StageInfo FindStageInfoForCollider(Collider collider)
+        private StageInfo FindStageInfoForCollider(Collider collider)
         {
             Transform t = collider.transform;
             StageInfo stageInfo = t.GetComponent<StageInfo>();
@@ -194,178 +174,6 @@ namespace Run4YourLife.Player
             }
             return stageInfo;
         }
-
-        private static Collider LeftMostCollider(Collider[] colliders)
-        {
-            Collider leftCollider = null;
-            float xOffset = Mathf.Infinity;
-            foreach (Collider c in colliders)
-            {
-                if (c.bounds.center.x < xOffset)
-                {
-                    xOffset = c.bounds.center.x;
-                    leftCollider = c;
-                }
-            }
-
-            return leftCollider;
-        }
-
-        private void SpawnUnder(Collider collider, ref SkillSpawnData skillSpawnData)
-        {
-            Vector3 newPos = transform.position;
-            newPos.y = collider.bounds.min.y;
-            skillSpawnData.position = newPos;
-            Transform actualParent = collider.transform.parent;
-            while (actualParent.tag == Tags.Wall)
-            {
-                actualParent = actualParent.parent;
-            }
-            skillSpawnData.parent = actualParent;
-        }*/
-
-        protected override void ResetState()
-        {
-            StopAllCoroutines();
-            m_warningSpikeParticles.SetActive(false);
-            m_earthSpikeChild.SetActive(false);
-        }
-
-        /*protected override void OnSkillStart()
-        {
-            float radius = width / 2;
-            Vector3 centerPos = transform.position;
-            centerPos.y += offset;//Added offset to check
-            float leftMax = CheckForGround(new Vector3(centerPos.x - radius, centerPos.y, centerPos.z), centerPos, false);
-            float rightMax = CheckForGround(centerPos, new Vector3(centerPos.x + radius, centerPos.y, centerPos.z), true);
-
-            float xOffset = leftMax - centerPos.x + rightMax - centerPos.x;
-            transform.Translate(new Vector3(xOffset / 4, 0, 0));
-
-            float maxLenght = rightMax - leftMax;
-            maxPercent = maxLenght / width;
-
-            StartCoroutine(StartAdvise());
-        }
-
-        private float CheckForGround(Vector3 min, Vector3 max, bool takeMax)
-        {
-            return CheckForGround(min, max, takeMax, 1);
-        }
-
-        private float CheckForGround(Vector3 min, Vector3 max, bool takeMax, int iterationNumber)
-        {
-            bool minCheck = takeMax;
-            bool maxCheck = !takeMax;
-            minCheck |= Physics.Raycast(min, Vector3.down, offset * 2, Layers.Stage, QueryTriggerInteraction.Ignore);
-            maxCheck |= Physics.Raycast(max, Vector3.down, offset * 2, Layers.Stage, QueryTriggerInteraction.Ignore);
-
-            if (minCheck && maxCheck)
-            {
-                if (takeMax)
-                {
-                    return max.x;
-                }
-                else
-                {
-                    return min.x;
-                }
-            }
-            else if (iterationNumber > maximumIterNumToCheck)//Exit if to many iterations
-            {
-                if (minCheck)
-                {
-                    return min.x;
-                }
-                else
-                {
-                    return max.x;
-                }
-            }
-            else
-            {
-                if (minCheck)
-                {
-                    Vector3 mid = max;
-                    float radius = width / 2;
-                    float actualRadiusCheck = radius / (2 * iterationNumber);
-                    mid.x -= actualRadiusCheck;
-                    if (Physics.Raycast(mid, Vector3.down, offset * 2, Layers.Stage, QueryTriggerInteraction.Ignore))
-                    {
-                        return CheckForGround(mid, max, takeMax, ++iterationNumber);
-                    }
-                    return CheckForGround(min, mid, takeMax, ++iterationNumber);
-                }
-                else
-                {
-                    Vector3 mid = min;
-                    float radius = width / 2;
-                    float actualRadiusCheck = radius / (2 * iterationNumber);
-                    mid.x += actualRadiusCheck;
-                    if (Physics.Raycast(mid, Vector3.down, offset, Layers.Stage, QueryTriggerInteraction.Ignore))
-                    {
-                        return CheckForGround(min, mid, takeMax, ++iterationNumber);
-                    }
-                    return CheckForGround(mid, max, takeMax, ++iterationNumber);
-                }
-            }
-        }
-        
-        IEnumerator StartAdvise()
-        {
-            AudioManager.Instance.PlaySFX(m_skillTriggerClip);
-            adviseParticles.transform.localScale = new Vector3(maxPercent * width, maxPercent * width, maxPercent * width);
-            adviseParticles.SetActive(true);
-            yield return new WaitForSeconds(delayHit);
-            adviseParticles.SetActive(false);
-            StartCoroutine(StartPikeGrow());
-        }
-
-        IEnumerator StartPikeGrow()
-        {
-            TrembleManager.Instance.Tremble(trembleConfig);
-            Vector3 currentScale = transform.localScale = Vector3.zero;
-            earthPikeEffect.SetActive(true);
-            float growBySecond = maxPercent / timeToGrow;
-            Vector3 increaseVector = new Vector3(growBySecond, growBySecond, growBySecond);
-            while (CheckForSpace() && currentScale.magnitude < maxPercent)
-            {
-                currentScale += increaseVector * Time.deltaTime;
-                transform.localScale = currentScale * width;
-                transform.Translate(0, 0, 0);
-                yield return null;
-            }
-            yield return new WaitForSeconds(timeToBreak);
-            earthPikeEffect.SetActive(false);
-            Break();
-        }
-
-        private bool CheckForSpace()
-        {
-            bool ret = false;
-            RaycastHit info;
-            ret |= Physics.Raycast(stageCollider.bounds.center, Vector3.right, out info, stageCollider.bounds.extents.x, Layers.Stage, QueryTriggerInteraction.Ignore);
-            ret |= Physics.Raycast(stageCollider.bounds.center, Vector3.left, out info, stageCollider.bounds.extents.x, Layers.Stage, QueryTriggerInteraction.Ignore);
-            ret |= Physics.Raycast(stageCollider.bounds.center, Vector3.up, out info, stageCollider.bounds.extents.y, Layers.Stage, QueryTriggerInteraction.Ignore);
-
-            return !ret;
-        }
-
-        private void Break()
-        {
-            if (phase != SkillBase.Phase.PHASE1)
-            {
-                GameObject instance = DynamicObjectsManager.Instance.GameObjectPool.GetAndPosition(wallGameObject, transform.position, Quaternion.identity, true);
-                Vector3 currentScale = new Vector3(maxPercent, maxPercent, maxPercent);
-                instance.transform.localScale = currentScale * width;
-                instance.transform.SetParent(transform.parent);
-            }
-
-            //We should make a copy of the pike, but allready broken, and break it here
-
-            gameObject.SetActive(false);
-        }
-        */
 
         protected override void OnSkillStart()
         {
@@ -378,22 +186,22 @@ namespace Run4YourLife.Player
 
             // Display Ground Particles for a short amount of time
             m_warningSpikeParticles.SetActive(true);
-            yield return new WaitForSeconds(delayHit);
+            yield return new WaitForSeconds(m_warningParticlesDuration);
             m_warningSpikeParticles.SetActive(false);
 
             TrembleManager.Instance.Tremble(m_spikeGrowTrembleConfig);
 
             // Earth spike grows
             m_earthSpikeChild.SetActive(true);
-            float endTime = Time.time + m_timeToGrow;
+            float endTime = Time.time + m_earthSpikeGrowthDuration;
             while (Time.time < endTime)
             {
-                float percentage = 1f - ((endTime - Time.time) / m_timeToGrow);
+                float percentage = 1f - ((endTime - Time.time) / m_earthSpikeGrowthDuration);
                 transform.localScale = Vector3.one * percentage;
                 yield return null;
             }
 
-            yield return new WaitForSeconds(m_timeToBreak);
+            yield return new WaitForSeconds(m_breakEarthSpikeDelay);
             m_earthSpikeChild.SetActive(false);
 
             SpawnBreakableWall();
