3e99056d58f6ee0ecbb5ffff228c61e09c3204ff
gerardpf2 <gerard.contacte@gmail.com>
2018-07-20 15:55:28 +0200

    New CharacterSelection added game start logic

--- Run4YourLife/Assets/Code/SceneSpecific/CharacterSelection/New/New_PlayerStandsManager.cs
+++ Run4YourLife/Assets/Code/SceneSpecific/CharacterSelection/New/New_PlayerStandsManager.cs
@@ -44,6 +44,9 @@ namespace Run4YourLife.SceneSpecific.CharacterSelection
 
         private Dictionary<PlayerHandle, bool> playerSelectionDone = new Dictionary<PlayerHandle, bool>();
 
+        private bool isGameReady = false;
+        private bool gameStarting = false;
+
         void Awake()
         {
             playerPrefabManager = GetComponent<New_PlayerPrefabManager>();
@@ -61,6 +64,7 @@ namespace Run4YourLife.SceneSpecific.CharacterSelection
 
         void Start() // OnEnable
         {
+            UpdateGameReady();
             PlayerManager.Instance.OnPlayerChanged.AddListener(OnPlayerAdded);
         }
 
@@ -69,13 +73,68 @@ namespace Run4YourLife.SceneSpecific.CharacterSelection
             PlayerManager.Instance.OnPlayerChanged.RemoveListener(OnPlayerAdded);
         }
 
+        #region Transitions
+
         public void OnGoMainMenu()
         {
+            if(gameStarting)
+            {
+                return;
+            }
+
             CharacterSelectionManager.Instance.OnMainMenuStart();
+        }
+
+        public void OnGoGame()
+        {
+            if(gameStarting)
+            {
+                return;
+            }
+
+            if(isGameReady)
+            {
+                gameStarting = true;
+
+                PreparePlayers();
+                CharacterSelectionManager.Instance.OnGameStart();
+            }
+        }
+
+        #endregion
+
+        public bool CanRotate(PlayerHandle playerHandle)
+        {
+            if(gameStarting)
+            {
+                return false;
+            }
+
+            return playerSelectionDone[playerHandle];
+        }
+
+        public string GetAnimationNameOnSelected(PlayerHandle playerHandle)
+        {
+            return playersCurrentCell[playerHandle].animationNameOnSelected;
+        }
+
+        public string GetAnimationNameOnNotSelected(PlayerHandle playerHandle)
+        {
+            return playersCurrentCell[playerHandle].animationNameOnNotSelected;
+        }
+
+        public void UpdateBackFillAmount()
+        {
+            back.fillAmount = backControlScheme.GetPercent();
         }
 
         private void OnPlayerAdded(PlayerHandle playerHandle)
         {
+            if(gameStarting)
+            {
+                return;
+            }
+
             FillStand(playerHandle);
 
             playerIds.Add(playerHandle, currentId++);
@@ -166,10 +225,88 @@ namespace Run4YourLife.SceneSpecific.CharacterSelection
             return runnerCellDatas[0];
         }
 
+        private void UpdateGameReady()
+        {
+            isGameReady = IsGameReady();
+
+            if(isGameReady)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+        private bool IsGameReady()
+        {
+            // 2 or more players
+
+            if(currentId < 2)
+            {
+                return false;
+            }
+
+            // All players have done their selection
+
+            foreach(KeyValuePair<PlayerHandle, bool> currentPlayerSelectionDone in playerSelectionDone)
+            {
+                if(!currentPlayerSelectionDone.Value)
+                {
+                    return false;
+                }
+            }
+
+            // There is just one boss
+
+            bool bossFound = false;
+
+            foreach(KeyValuePair<PlayerHandle, New_CellData> currentPlayerCurrentCell in playersCurrentCell)
+            {
+                bool isBoss = currentPlayerCurrentCell.Value.isBoss;
+
+                if(isBoss)
+                {
+                    if(bossFound)
+                    {
+                        return false;
+                    }
+
+                    bossFound = true;
+                }
+            }
+
+            return bossFound;
+        }
+
+        private void PreparePlayers()
+        {
+            PlayerHandle bossHandle = null;
+
+            foreach(KeyValuePair<PlayerHandle, New_CellData> currentPlayerCurrentCell in playersCurrentCell)
+            {
+                currentPlayerCurrentCell.Key.IsBoss = currentPlayerCurrentCell.Value.isBoss;
+                currentPlayerCurrentCell.Key.CharacterType = currentPlayerCurrentCell.Value.characterType;
+
+                if(currentPlayerCurrentCell.Key.IsBoss)
+                {
+                    bossHandle = currentPlayerCurrentCell.Key;
+                }
+            }
+
+            PlayerManager.Instance.SetPlayerAsBoss(bossHandle);
+        }
+
         #region Player input management
 
         public RequestCompletionState OnPlayerInputSelect(PlayerHandle playerHandle)
         {
+            if(gameStarting)
+            {
+                return RequestCompletionState.Error;
+            }
+
             if(playerSelectionDone[playerHandle])
             {
                 return RequestCompletionState.Unmodified;
@@ -187,11 +324,18 @@ namespace Run4YourLife.SceneSpecific.CharacterSelection
 
             playerSelectionDone[playerHandle] = true;
 
+            UpdateGameReady();
+
             return RequestCompletionState.Completed;
         }
 
         public RequestCompletionState OnPlayerInputUnselect(PlayerHandle playerHandle)
         {
+            if(gameStarting)
+            {
+                return RequestCompletionState.Error;
+            }
+
             if(!playerSelectionDone[playerHandle])
             {
                 return RequestCompletionState.Unmodified;
@@ -199,11 +343,18 @@ namespace Run4YourLife.SceneSpecific.CharacterSelection
 
             playerSelectionDone[playerHandle] = false;
 
+            UpdateGameReady();
+
             return RequestCompletionState.Completed;
         }
 
         public RequestCompletionState OnPlayerInputUp(PlayerHandle playerHandle)
         {
+            if(gameStarting)
+            {
+                return RequestCompletionState.Error;
+            }
+
             if(playerSelectionDone[playerHandle])
             {
                 return RequestCompletionState.Error;
@@ -214,6 +365,11 @@ namespace Run4YourLife.SceneSpecific.CharacterSelection
 
         public RequestCompletionState OnPlayerInputDown(PlayerHandle playerHandle)
         {
+            if(gameStarting)
+            {
+                return RequestCompletionState.Error;
+            }
+
             if(playerSelectionDone[playerHandle])
             {
                 return RequestCompletionState.Error;
@@ -224,6 +380,11 @@ namespace Run4YourLife.SceneSpecific.CharacterSelection
 
         public RequestCompletionState OnPlayerInputLeft(PlayerHandle playerHandle)
         {
+            if(gameStarting)
+            {
+                return RequestCompletionState.Error;
+            }
+
             if(playerSelectionDone[playerHandle])
             {
                 return RequestCompletionState.Error;
@@ -234,6 +395,11 @@ namespace Run4YourLife.SceneSpecific.CharacterSelection
 
         public RequestCompletionState OnPlayerInputRight(PlayerHandle playerHandle)
         {
+            if(gameStarting)
+            {
+                return RequestCompletionState.Error;
+            }
+
             if(playerSelectionDone[playerHandle])
             {
                 return RequestCompletionState.Error;
@@ -243,25 +409,5 @@ namespace Run4YourLife.SceneSpecific.CharacterSelection
         }
 
         #endregion
-
-        public bool CanRotate(PlayerHandle playerHandle)
-        {
-            return playerSelectionDone[playerHandle];
-        }
-
-        public string GetAnimationNameOnSelected(PlayerHandle playerHandle)
-        {
-            return playersCurrentCell[playerHandle].animationNameOnSelected;
-        }
-
-        public string GetAnimationNameOnNotSelected(PlayerHandle playerHandle)
-        {
-            return playersCurrentCell[playerHandle].animationNameOnNotSelected;
-        }
-
-        public void UpdateBackFillAmount()
-        {
-            back.fillAmount = backControlScheme.GetPercent();
-        }
     }
 }
