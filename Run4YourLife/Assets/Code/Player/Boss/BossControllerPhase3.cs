579211c7880fe0f00e85309ef8e51dc7684b35b8
Pere Viader <pere.viader22@gmail.com>
2018-07-22 14:02:55 +0200

    Implementing phase 3 boss #470

---
+++ Run4YourLife/Assets/Code/Player/Boss/BossControllerPhase3.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Run4YourLife.GameManagement.AudioManagement;
+using Run4YourLife.Utils;
+using Run4YourLife.GameManagement;
+using Run4YourLife.CameraUtils;
+using Run4YourLife.Player.Boss;
+
+namespace Run4YourLife.Player
+{
+    public class BossControllerPhase3 : BossController
+    {
+        [SerializeField]
+        private float m_fireLaserNormalizedTime;
+
+        [SerializeField]
+        private GameObject m_laserPrefab;
+
+        protected override void ExecuteShoot() //TODO: Move strings to BossAnimation
+        {
+            base.ExecuteShoot();
+
+            m_animator.SetTrigger("Shoot");
+            StartCoroutine(AnimationCallbacks.OnStateAtNormalizedTime(m_animator, BossAnimation.Triggers.Shoot, m_fireLaserNormalizedTime, () => ExecuteShootCallback()));
+        }
+
+        private void ExecuteShootCallback()
+        {
+            AudioManager.Instance.PlaySFX(m_shotClip);
+
+            Vector3 director = (m_crossHairControl.Position - m_shotSpawn.position).normalized;
+            Quaternion rotation = Quaternion.FromToRotation(Vector3.forward, director);
+            DynamicObjectsManager.Instance.GameObjectPool.GetAndPosition(m_laserPrefab, m_shotSpawn.position, rotation, true);
+        }
+
+        protected override void ExecuteMelee()
+        {
+            base.ExecuteMelee();
+
+            m_animator.SetTrigger(BossAnimation.Triggers.Melee);
+            AudioManager.Instance.PlaySFX(m_meleeClip);
+        }
+    }
+}
---
+++ Run4YourLife/Assets/Code/Player/Boss/Shoot/LaserBullet/BossLaserBulletController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Run4YourLife.Player.Boss
+{
+    public class BossLaserBulletController : MonoBehaviour
+    {
+        [SerializeField]
+        private float m_chargeDuration;
+
+        [SerializeField]
+        private float m_laserDuration;
+
+        [SerializeField]
+        private float m_maxLaserDistance;
+
+        [SerializeField]
+        [Tooltip("Size of the laser killing collision, imagine a X=length, Y=ratius")]
+        private float m_laserCollisionWidth;
+
+        [SerializeField]
+        private FXReceiver m_chargeLaserReceiver;
+
+        [SerializeField]
+        private FXReceiver m_laserReceiver;
+
+        private void OnEnable()
+        {
+            StartCoroutine(LaserBehaviour());
+        }
+
+        private void OnDisable()
+        {
+            ResetState();
+            StopAllCoroutines();
+        }
+
+        private void ResetState()
+        {
+        }
+
+        private IEnumerator LaserBehaviour()
+        {
+            // Charge Laser
+            m_chargeLaserReceiver.PlayFx();
+            yield return new WaitForSeconds(m_chargeDuration);
+
+            // Fire Laser
+            Vector3 laserEnd = LocateEndingLaserPosition();
+            float laserDistance = Vector3.Distance(transform.position, laserEnd);
+            
+            Vector3 center = transform.position + transform.forward * laserDistance/2f;
+            Vector3 halfExtents = new Vector3(m_laserCollisionWidth/2f, m_laserCollisionWidth/2f, laserDistance/2f);
+
+            m_laserReceiver.PlayFx();
+
+            // Check for players to kill
+            Collider[] runnersHit = Physics.OverlapBox(center, halfExtents, transform.rotation, Layers.Runner, QueryTriggerInteraction.Ignore);
+            foreach (Collider runnerHit in runnersHit)
+            {
+                IRunnerEvents runnerEvents = runnerHit.GetComponent<IRunnerEvents>();
+                Debug.Assert(runnerEvents != null);
+                if(runnerEvents != null)
+                {
+                    runnerEvents.Kill();
+                }
+            }
+
+            yield return new WaitForSeconds(m_laserDuration);
+            gameObject.SetActive(false);
+        }
+
+        private Vector3 LocateEndingLaserPosition()
+        {
+            Vector3 position;
+            RaycastHit raycastHit;
+            if(Physics.Raycast(transform.position, transform.forward, out raycastHit, m_maxLaserDistance, Layers.Stage, QueryTriggerInteraction.Ignore))
+            {
+                position = raycastHit.point;
+            }
+            else
+            {
+                position = transform.position + transform.forward * m_maxLaserDistance;
+            }
+            return position;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Vector3 center = transform.position + transform.forward * m_maxLaserDistance/2f;
+            Vector3 size = new Vector3(m_laserCollisionWidth, m_laserCollisionWidth, m_maxLaserDistance);
+            Gizmos.DrawWireCube(center, size);
+        }
+    }
+}
