da618bd03a94482b67922c459417333c29597db3
Pere Viader <pere.viader22@gmail.com>
2018-07-25 11:05:58 +0200

    Refactoring boss destruction system. Improved functionality and made it simpler

--- Run4YourLife/Assets/Code/SceneSpecific/GameScene/BossDestroy/BossDestructibleBase.cs
+++ Run4YourLife/Assets/Code/SceneSpecific/GameScene/BossDestroy/BossDestructibleBase.cs
@@ -9,10 +9,10 @@ namespace Run4YourLife.GameManagement.Refactor
         private float m_distance;
 
         [SerializeField]
-        private bool m_isStatic;
+        private bool m_isStatic = true;
 
         [SerializeField]
-        private bool m_canBeRegenerated;
+        private bool m_canBeRegenerated = true;
 
         private BossDestructibleState m_bossDestructibleState;
 
