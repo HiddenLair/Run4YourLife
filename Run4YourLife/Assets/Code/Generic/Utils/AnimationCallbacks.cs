2cadb1e34b745e8d1cb23c027d975aef557ec844
Moaif <32565030+Moaif@users.noreply.github.com>
2018-08-02 11:50:57 +0200

    Fixed laser diferent behaviour depending on animation, and some parameters

--- Run4YourLife/Assets/Code/Generic/Utils/AnimationCallbacks.cs
+++ Run4YourLife/Assets/Code/Generic/Utils/AnimationCallbacks.cs
@@ -44,14 +44,30 @@ namespace Run4YourLife.Utils
         public static IEnumerator AfterStateAtNormalizedTime(Animator anim, string state, float time, Action action, bool infinite = false)
         {
             WaitUntil waitUntilState = new WaitUntil(() => anim.GetCurrentAnimatorStateInfo(0).IsName(state));
-            WaitUntil waitUntilTransition = new WaitUntil(() => anim.IsInTransition(0));
-            WaitUntil waitUntilNormalizedTime = new WaitUntil(() => Mathf.Repeat(anim.GetCurrentAnimatorStateInfo(0).normalizedTime * anim.GetCurrentAnimatorStateInfo(0).speed, 1.0f) >= time);
+            WaitUntil waitUntilTransition = new WaitUntil(() => anim.IsInTransition(0));          
 
             do
             {
                 yield return waitUntilState;
                 yield return waitUntilTransition;
-                yield return waitUntilNormalizedTime;
+                while (true)
+                {
+                    if (anim.IsInTransition(0))//While in transition, we need to look at normalized time in the next state, but when transition ends, the "next" state, is the current one
+                    {
+                        if(Mathf.Repeat(anim.GetNextAnimatorStateInfo(0).normalizedTime * anim.GetNextAnimatorStateInfo(0).speed, 1.0f) <= time)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (Mathf.Repeat(anim.GetCurrentAnimatorStateInfo(0).normalizedTime * anim.GetCurrentAnimatorStateInfo(0).speed, 1.0f) <= time)
+                        {
+                            break;
+                        }
+                    }
+                    yield return null;
+                }
                 action.Invoke();
             } while (infinite);
         }
