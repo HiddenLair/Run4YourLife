80f648d3d3c770b152c3883e2aa5707a3c80e99c
Pere Viader <pere.viader22@gmail.com>
2018-08-08 19:23:53 +0200

    Fixing falling platform spikes. Platform was using a non kinematic rigidbody and could randomly move weirdly.

--- Run4YourLife/Assets/Code/Interactables/FallingPlatform/FallingPlatformController.cs
+++ Run4YourLife/Assets/Code/Interactables/FallingPlatform/FallingPlatformController.cs
@@ -1,60 +1,93 @@
-﻿using UnityEngine;
-using Run4YourLife;
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
 using Run4YourLife.Utils;
 using Run4YourLife.GameManagement;
+using Run4YourLife.GameManagement.AudioManagement;
 
 namespace Run4YourLife.Interactables
 {
     [RequireComponent(typeof(Rigidbody))]
     public class FallingPlatformController : MonoBehaviour, IBossDestructibleNotified
     {
+        [SerializeField]
+        private float m_startingSpeed;
 
         [SerializeField]
-        private GameObject m_FallingCollider;
+        private float m_gravity;
 
         [SerializeField]
         public float m_delay;
 
-        private Rigidbody m_rigidbody;
+        [SerializeField]
+        private TrembleConfig m_collideGroundTrembleConfig;
+
+        [SerializeField]
+        private AudioClip m_collideGroundSFX;
 
         private Animator m_animator;
 
-        private bool activatedFlag = false;
+        private float m_currentSpeed;
+        private bool m_activated;
         private Vector3 m_startingPosition;
 
         private void Awake()
         {
             m_animator = GetComponentInChildren<Animator>();
-            m_rigidbody = GetComponent<Rigidbody>();
-            Debug.Assert(m_rigidbody != null);
-            m_startingPosition = m_rigidbody.transform.localPosition;
+            m_startingPosition = transform.localPosition;
+            m_currentSpeed = m_startingSpeed;
         }
 
         public void OnRunnerWalkedOnTop()
         {
-            if (!activatedFlag)
+            if (!m_activated)
             {
+                m_activated = true;
                 StartCoroutine(YieldHelper.WaitForSeconds(Fall, m_delay));
             }
         }
 
         private void Fall()
         {
+            StartCoroutine(FallBehaviour());
+        }
+
+        private IEnumerator FallBehaviour()
+        {
             m_animator.enabled = false;
-            m_rigidbody.useGravity = true;
-            m_rigidbody.isKinematic = false;
-            m_FallingCollider.SetActive(true);
-            activatedFlag = true;
+            yield return new WaitForSeconds(m_delay);
+
+            Vector3 endPosition = FindEndPosition();
+            while (transform.position != endPosition)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, endPosition, m_currentSpeed * Time.deltaTime);
+                m_currentSpeed += m_gravity * Time.deltaTime;
+                yield return null;
+            }
+
+            //Collide ground
+            TrembleManager.Instance.Tremble(m_collideGroundTrembleConfig);
+            AudioManager.Instance.PlaySFX(m_collideGroundSFX);
+        }
+
+        private Vector3 FindEndPosition()
+        {
+            RaycastHit info;
+            bool found = Physics.Raycast(transform.position, Vector3.down, out info, Mathf.Infinity, Layers.Stage);
+            Debug.Assert(found);
+            return info.point;
         }
 
         private void ResetState()
         {
-            m_rigidbody.transform.localPosition = m_startingPosition;
+            StopAllCoroutines();
 
-            m_rigidbody.useGravity = false;
-            m_rigidbody.isKinematic = true;
-            m_FallingCollider.SetActive(false);
-            activatedFlag = false;
+            m_animator.enabled = true;
+            transform.localPosition = m_startingPosition;
+            m_activated = false;
+            m_currentSpeed = m_startingSpeed;
         }
 
         void IBossDestructibleNotified.OnDestroyed()
