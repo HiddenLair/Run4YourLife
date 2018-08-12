e1f68727c3800e0d96c193ec2f5604ed76dcf78a
ZelleryonDesktop <xavibravo.guillen@gmail.com>
2018-08-12 16:24:37 +0200

    Added small animation to floating platforms on phase 3 with small delay between them

---
+++ Run4YourLife/Assets/Code/Generic/Utils/AnimationStartDelayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Run4YourLife.Utils
+{
+    public class AnimationStartDelayer : MonoBehaviour
+    {
+        [SerializeField]
+        private Animator animator;
+
+        [SerializeField]
+        private string AnimatorStateName;
+
+        [SerializeField]
+        private float delay;
+
+        [SerializeField]
+        private bool randomize = false;
+
+        void Start()
+        {
+            float delayTime;
+
+            if(randomize)
+            {
+                delayTime = Random.Range(0.0f, 1.5f);
+            }
+            else
+            {
+                delayTime = delay;
+            }
+
+            animator.Play(AnimatorStateName, -1, delayTime);
+        }
+    }
+}
