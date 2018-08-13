65806606988c4587a2be4d5bb5e50fe3ecf4dd82
Moaif <32565030+Moaif@users.noreply.github.com>
2018-08-13 10:12:14 +0200

    Setted audio listener in game plane #579

---
+++ Run4YourLife/Assets/Code/Generic/Utils/Positioning/TransformLockedInPosition.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Run4YourLife.Utils
+{
+    public class TransformLockedInPosition : MonoBehaviour
+    {
+        #region Inspector
+        [SerializeField]
+        private Vector3 worldPosition;
+
+        [SerializeField]
+        private bool setX;
+
+        [SerializeField]
+        private bool setY;
+
+        [SerializeField]
+        private bool setZ;
+        #endregion
+
+        #region Variables
+
+        private Transform m_transform;
+
+        #endregion
+
+        private void Awake()
+        {
+            m_transform = GetComponent<Transform>();
+            UpdatePosition();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            UpdatePosition();
+        }
+
+        private void UpdatePosition()
+        {
+            Vector3 tempPosition = m_transform.position;
+            if (setX)
+            {
+                tempPosition.x = worldPosition.x;
+            }
+            if (setY)
+            {
+                tempPosition.y = worldPosition.y;
+            }
+            if (setZ)
+            {
+                tempPosition.z = worldPosition.z;
+            }
+            m_transform.position = tempPosition;
+        }
+    }
+}
