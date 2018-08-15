bd93014bf438a258a946c6a3aacd2ffc3f4c9d46
Moaif <32565030+Moaif@users.noreply.github.com>
2018-08-15 12:31:48 +0200

    Added particles and sound to goThrough platform #559  #554

---
+++ Run4YourLife/Assets/Code/Generic/Utils/OnInteractPlayFx.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnInteractPlayFx : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip("Optional field")]
+    private string tagToCheck;
+
+    [SerializeField]
+    private FXReceiver receiver;
+
+    [SerializeField]
+    private bool setParticlesParent;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(tagToCheck == null || other.tag == tagToCheck)
+        {
+            receiver.PlayFx(setParticlesParent);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (tagToCheck == null || collision.collider.tag == tagToCheck)
+        {
+            receiver.PlayFx(setParticlesParent);
+        }
+    }
+}
