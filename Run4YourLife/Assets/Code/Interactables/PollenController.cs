7498a99f7d84bc2ad161cce3fb8687292f2beb53
Pere Viader <pere.viader22@gmail.com>
2018-08-17 09:59:53 +0200

    Fixed pollen particles disappearing suddenly #598

--- Run4YourLife/Assets/Code/Interactables/PollenController.cs
+++ Run4YourLife/Assets/Code/Interactables/PollenController.cs
@@ -4,27 +4,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Run4YourLife.Player.Runner;
+using System;
 
 namespace Run4YourLife.Interactables.Pollen
 {
     public class PollenController : MonoBehaviour
     {
+        [SerializeField]
+        private Collider m_collider;
+
+        private ParticleSystem[] m_particleSystems;
+
+        private bool m_isDisappearing;
+
         private float m_endTime;
+
+        private void Awake()
+        {
+            m_particleSystems = GetComponentsInChildren<ParticleSystem>();
+        }
+
+        private void OnEnable()
+        {
+            ResetState();
+        }
+
+        private void ResetState()
+        {
+            m_endTime = 0f;
+            m_isDisappearing = false;
+            m_collider.enabled = true;
+        }
+
+        private void Disappear()
+        {
+            m_isDisappearing = true;
+            m_collider.enabled = false;
+
+            foreach (ParticleSystem p in m_particleSystems)
+            {
+                p.Stop(false, ParticleSystemStopBehavior.StopEmitting);
+            }
+        }
 
         public void SetTimeAlive(float duration)
         {
             m_endTime = Time.time + duration;
         }
 
-        public void OnCollidedWithRunner(RunnerController runnerController)
+        private void OnCollidedWithRunner(RunnerController runnerController)
         {
             runnerController.Kill();
             Disappear();
-        }
-
-        public void Disappear()
-        {
-            gameObject.SetActive(false);
         }
 
         private void OnTriggerEnter(Collider other)
@@ -38,10 +69,33 @@ namespace Run4YourLife.Interactables.Pollen
 
         private void Update()
         {
-            if (Time.time >= m_endTime)
+            if (!m_isDisappearing)
             {
-                Disappear();
+                if (Time.time >= m_endTime)
+                {
+                    Disappear();
+                }
             }
+            else
+            {
+                if (!AreParticleSystemsAlive())
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+
+        private bool AreParticleSystemsAlive()
+        {
+            foreach (ParticleSystem p in m_particleSystems)
+            {
+                if (p.IsAlive(false))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
