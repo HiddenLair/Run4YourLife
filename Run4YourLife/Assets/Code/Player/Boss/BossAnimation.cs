3b6920bf2a900c1a7b51d04edeaa7251d4cda834
Pere Viader <pere.viader22@gmail.com>
2018-08-18 11:28:39 +0200

    Implementing boss win animation into boss win scene #608

--- Run4YourLife/Assets/Code/Player/Boss/BossAnimation.cs
+++ Run4YourLife/Assets/Code/Player/Boss/BossAnimation.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 namespace Run4YourLife.Player
 {
-    public class BossAnimation {
+    public class BossAnimation
+    {
 
-        public class StateNames {
+        public class StateNames
+        {
             public static readonly string Move = "Move";
             public static readonly string Shoot = "Shoot";
             public static readonly string Laugh = "Laugh";
@@ -16,15 +18,18 @@ namespace Run4YourLife.Player
             public static readonly string Cast = "Cast";
             public static readonly string ChargeShoot = "ChargeShoot";
             public static readonly string ThreateningScream = "Threatening Scream";
+            public static readonly string ChestBeat = "ChestBeat";
         }
 
-        public class Triggers {
+        public class Triggers
+        {
             public static readonly string Shoot = "Shoot";
             public static readonly string Mele = "Mele";
             public static readonly string MeleR = "MeleR";
             public static readonly string MeleL = "MeleL";
             public static readonly string Cast = "Casting";
             public static readonly string Laugh = "Laugh";
+            public static readonly string ChestBeat = "ChestBeat";
         }
     }
 }
