99c61123a2860322aa81418fed4b698fb86d16f1
Moaif <32565030+Moaif@users.noreply.github.com>
2018-09-19 19:31:01 +0200

    Updated Credits functionality

---
+++ Run4YourLife/Assets/Code/UI/CreditsSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Run4YourLife.UI {
+    public class CreditsSwitcher : MonoBehaviour {
+
+        [SerializeField]
+        private GameObject optionsCanvas;
+
+        [SerializeField]
+        private GameObject creditsCanvas;
+
+        private bool switchState = true;
+
+        public void Switch()
+        {
+            switchState = !switchState;
+            optionsCanvas.SetActive(switchState);
+            creditsCanvas.SetActive(!switchState);
+        }
+
+    }
+}
